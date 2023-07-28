5502b04c5660e452cc8b857520dcd902e7ccfd3e
GUNDY <106914201+Gundy93@users.noreply.github.com>
2023-07-29 01:05:33 +0900

    Create 문자 개수 세기.swift

---
+++ Gundy/Programmers/LV. 0/문자 개수 세기.swift
@@ -0,0 +1,7 @@
+func solution(_ my_string:String) -> [Int] {
+    var count = [Character: Int]()
+    for word in my_string {
+        count[word, default: 0] += 1
+    }
+    return ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"].map { count[$0, default: 0] }
+}
