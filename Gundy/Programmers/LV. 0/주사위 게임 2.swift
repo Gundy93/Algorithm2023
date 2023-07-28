39acdb239286f13470d8e32f3430009c571727e3
GUNDY <106914201+Gundy93@users.noreply.github.com>
2023-07-28 12:17:17 +0900

    Create 주사위 게임 2.swift

---
+++ Gundy/Programmers/LV. 0/주사위 게임 2.swift
@@ -0,0 +1,8 @@
+func solution(_ a:Int, _ b:Int, _ c:Int) -> Int {
+    if a != b, b != c, a != c {
+        return a + b + c
+    } else if a == b, b == c {
+        return (a + b + c) * (a * a + b * b + c * c) * (a * a * a + b * b * b + c * c * c)
+    }
+    return (a + b + c) * (a * a + b * b + c * c)
+}
