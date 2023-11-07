59fe1493394363ab31a31c662ac122b14a63f65b
GUNDY <106914201+Gundy93@users.noreply.github.com>
2023-11-07 16:28:29 +0900

    Update 9498_시험 성적.swift

--- Gundy/BAEKJOON/9498_시험 성적.swift
+++ Gundy/BAEKJOON/9498_시험 성적.swift
@@ -1,17 +1,17 @@
-func solution() {
-    let point: Int = Int(readLine()!)!
-    switch point {
-    case 90...100:
-        print("A")
-    case 80...89:
-        print("B")
-    case 70...79:
-        print("C")
-    case 60...69:
-        print("D")
-    default:
-        print("F")
-    }
+let point = Int(readLine()!)!
+var grade = String()
+
+switch point {
+case 90...:
+    grade = "A"
+case 80...:
+    grade = "B"
+case 70...:
+    grade = "C"
+case 60...:
+    grade = "D"
+default:
+    grade = "F"
 }
 
-solution()
+print(grade)
