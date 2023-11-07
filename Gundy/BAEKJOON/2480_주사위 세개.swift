5cb87bd50974bca2a492af7f17d8918cdb622247
GUNDY <106914201+Gundy93@users.noreply.github.com>
2023-11-07 16:57:50 +0900

    Update 2480_주사위 세개.swift

--- Gundy/BAEKJOON/2480_주사위 세개.swift
+++ Gundy/BAEKJOON/2480_주사위 세개.swift
@@ -6,6 +6,8 @@ if a == b, a == c {
     reward = 10000 + a * 1000
 } else if a == b {
     reward = 1000 + a * 100
+} else if a == c {
+    reward = 1000 + a * 100
 } else if b == c {
     reward = 1000 + b * 100
 } else {
