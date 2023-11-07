613f6bfaa82cd7364d8f00c01b9aad18bfb5c909
GUNDY <106914201+Gundy93@users.noreply.github.com>
2023-11-07 14:29:17 +0900

    Update 1475_방 번호.swift

--- Gundy/BAEKJOON/1475_방 번호.swift
+++ Gundy/BAEKJOON/1475_방 번호.swift
@@ -1,18 +1,7 @@
-func solution() {
-    let number: String = readLine()!
-    var result: [Character : Int] = [:]
-    number.forEach {
-        var text = $0
-        if $0 == "9" {
-            text = "6"
-        }
-        result[text, default: 0] += 1
-    }
-    if result["6", default: 0] % 2 == 1 {
-        result["6", default: 0] += 1
-    }
-    result["6", default: 0] = result["6", default: 0] / 2
-    print(result.values.sorted().last!)
-}
+let input = readLine()!
+let numbers = input.map { $0 == "9" ? 6 : Int(String($0))! }
+var counts = Array(repeating: 0, count: 9)
 
-solution()
+numbers.forEach { counts[$0] += 1 }
+counts[6] = counts[6] % 2 == 1 ? counts[6] / 2 + 1 : counts[6] / 2
+print(counts.max()!)
