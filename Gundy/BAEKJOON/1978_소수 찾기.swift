6b90430a6f74ab0819e6f87cf228a98fd966d7b1
GUNDY <106914201+Gundy93@users.noreply.github.com>
2023-11-14 16:51:42 +0900

    Update 1978_소수 찾기.swift

--- Gundy/BAEKJOON/1978_소수 찾기.swift
+++ Gundy/BAEKJOON/1978_소수 찾기.swift
@@ -1,17 +1,21 @@
 import Foundation
 
-func isPrime(number: Int) -> Bool {
-    guard number > 3 else { return number != 1 }
+let _ = readLine()
+let numbers = readLine()!.split(separator: " ").compactMap { Int($0) }
+let result = numbers.filter { number in
+    guard number != 1 else { return false }
+    
+    guard number >= 4 else { return true }
+    
     for divisor in 2...number {
+        guard Double(divisor) <= sqrt(Double(number)) else { break }
+        
         if number % divisor == 0 {
             return false
         }
-        guard Double(divisor) <= sqrt(Double(number)) else {
-            break
-        }
     }
+    
     return true
 }
 
-let _ = readLine()
-print(readLine()!.split(separator: " ").compactMap({ Int($0) }).filter({ isPrime(number: $0) }).count)
+print(result.count)
