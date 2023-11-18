3db41b8dbc8ca904027c47cad72def636f552676
GUNDY <106914201+Gundy93@users.noreply.github.com>
2023-11-18 20:27:41 +0900

    Update 7785_회사에 있는 사람.swift

--- Gundy/BAEKJOON/7785_회사에 있는 사람.swift
+++ Gundy/BAEKJOON/7785_회사에 있는 사람.swift
@@ -1,15 +1,14 @@
-var hash = Set<Substring>()
+let count = Int(readLine()!)!
+var people = Set<String>()
 
-for _ in 1...Int(readLine()!)! {
-    let command = readLine()!.split(separator: " ")
-    switch command[1] {
-    case "enter":
-        hash.insert(command[0])
-    default:
-        hash.remove(command[0])
+for _ in 1...count {
+    let log = readLine()!.split(separator: " ").map(String.init)
+    
+    if log[1] == "enter" {
+        people.insert(log[0])
+    } else {
+        people.remove(log[0])
     }
 }
 
-for value in hash.sorted(by: >) {
-    print(value)
-}
+print(people.sorted(by: >).joined(separator: "\n"))
