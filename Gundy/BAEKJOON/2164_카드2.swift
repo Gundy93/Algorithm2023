498a5cbc162436156027ef3855731c36d865c6a6
GUNDY <106914201+Gundy93@users.noreply.github.com>
2023-11-18 15:48:15 +0900

    Update 2164_카드2.swift

--- Gundy/BAEKJOON/2164_카드2.swift
+++ Gundy/BAEKJOON/2164_카드2.swift
@@ -1,12 +1,63 @@
-var cards = Array(1...Int(readLine()!)!)
-var index = 0
-
-while index < cards.count {
-    index += 1
-    if index < cards.count {
-        cards.append(cards[index])
-        index += 1
+struct Queue<T> {
+    
+    final class Node {
+        
+        let value: T
+        var next: Node?
+        
+        init(value: T) {
+            self.value = value
+        }
+    }
+    
+    private var head: Node?
+    private var tail: Node?
+    var first: T? {
+        return head?.value
+    }
+    var last: T? {
+        return tail?.value
+    }
+    
+    mutating func enqueue(_ newElement: T) {
+        let node = Node(value: newElement)
+        
+        guard let lastNode = tail else {
+            head = node
+            tail = node
+            
+            return
+        }
+        
+        lastNode.next = node
+        tail = node
+    }
+    
+    mutating func dequeue() -> T? {
+        let element = first
+        
+        if head?.next == nil {
+            tail = nil
+        }
+        
+        head = head?.next
+        
+        return element
     }
 }
 
-print(cards[cards.count - 1])
+var queue = Queue<Int>()
+let count = Int(readLine()!)!
+
+for number in 1...count {
+    queue.enqueue(number)
+}
+
+for _ in stride(from: 1, to: count, by: 1) {
+    let _ = queue.dequeue()
+    guard let element = queue.dequeue() else { break }
+    
+    queue.enqueue(element)
+}
+
+print(queue.last!)
