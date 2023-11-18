18e52b81d45d6684436ce41cf1b9f18b8a40bc51
GUNDY <106914201+Gundy93@users.noreply.github.com>
2023-11-18 20:14:24 +0900

    Update 10816_숫자 카드 2.swift

--- Gundy/BAEKJOON/10816_숫자 카드 2.swift
+++ Gundy/BAEKJOON/10816_숫자 카드 2.swift
@@ -1,46 +1,12 @@
 let _ = readLine()
-let numbers = readLine()!.split(separator: " ").compactMap({ Int($0) }).sorted()
+let cards = readLine()!.split(separator: " ").map(String.init)
+var counter = [String: Int]()
+
+for number in cards {
+    counter[number, default: 0] += 1
+}
+
 let _ = readLine()
-let targetNumbers = readLine()!.split(separator: " ").compactMap({ Int($0) })
-var result = [Int]()
+let result = readLine()!.split(separator: " ").map { String(counter[String($0), default: 0]) }.joined(separator: " ")
 
-for number in targetNumbers {
-    guard let firstIndex = firstIndex(of: number),
-          let lastIndex = lastIndex(of: number) else {
-        result.append(0)
-        continue
-    }
-    result.append(lastIndex - firstIndex + 1)
-}
-
-print(result.map({ String($0) }).joined(separator: " "))
-
-func firstIndex(of number: Int) -> Int? {
-    var start = 0
-    var end = numbers.count - 1
-    while start <= end {
-        let half = (start + end) / 2
-        if numbers[half] >= number {
-            end = half - 1
-        } else {
-            start = half + 1
-        }
-    }
-    guard start < numbers.count else { return nil }
-    return numbers[start] == number ? start : nil
-}
-
-func lastIndex(of number: Int) -> Int? {
-    var start = 0
-    var end = numbers.count - 1
-    while start <= end {
-        let half = (start + end) / 2
-        if numbers[half] <= number {
-            start = half + 1
-        } else {
-            end = half - 1
-        }
-    }
-    guard end >= 0 else { return nil }
-    return numbers[end] == number ? end : nil
-}
+print(result)
