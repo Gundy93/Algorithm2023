e0eb47e544068ae2b8101934d5b808ba08388927
GUNDY <106914201+Gundy93@users.noreply.github.com>
2023-11-18 12:01:39 +0900

    Update 18870_좌표 압축.swift

--- Gundy/BAEKJOON/18870_좌표 압축.swift
+++ Gundy/BAEKJOON/18870_좌표 압축.swift
@@ -1,21 +1,9 @@
 let _ = readLine()
-let targetNumbers = readLine()!.split(separator: " ").compactMap({ Int($0) })
-let numbers = Set(targetNumbers).sorted()
-let result = targetNumbers.compactMap({ firstIndex(of: $0) })
+let numbers = readLine()!.split(separator: " ").map { Int($0)! }
+var indices = [Int: Int]()
 
-print(result.map({ String($0) }).joined(separator: " "))
-
-func firstIndex(of number: Int) -> Int? {
-    var start = 0
-    var end = numbers.count - 1
-    while start <= end {
-        let half = (start + end) / 2
-        if numbers[half] >= number {
-            end = half - 1
-        } else {
-            start = half + 1
-        }
-    }
-    guard start < numbers.count else { return nil }
-    return numbers[start] == number ? start : nil
+for number in Set(numbers).sorted().enumerated() {
+    indices[number.element] = number.offset
 }
+
+print(numbers.map { String(indices[$0]!) }.joined(separator: " "))
