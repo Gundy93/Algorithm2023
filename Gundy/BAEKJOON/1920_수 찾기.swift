de8c92f254eddcad65654b5c68b9789950599958
GUNDY <106914201+Gundy93@users.noreply.github.com>
2023-11-18 20:07:04 +0900

    Update 1920_수 찾기.swift

--- Gundy/BAEKJOON/1920_수 찾기.swift
+++ Gundy/BAEKJOON/1920_수 찾기.swift
@@ -1,25 +1,17 @@
 let _ = readLine()
-let numbers = readLine()!.split(separator: " ").compactMap({ Int($0) }).sorted()
+let list = readLine()!.split(separator: " ").map { Int($0)! }
+var logger = [Int: Int]()
+
+for number in list {
+    logger[number] = 1
+}
+
 let _ = readLine()
-let result = readLine()!.split(separator: " ").compactMap({ Int($0) }).map({ isContains($0) ? 1 : 0 })
+let numbers = readLine()!.split(separator: " ").map { Int($0)! }
+var result = String()
 
-for number in result {
-    print(number)
+for number in numbers {
+    result += "\(logger[number] ?? 0)\n"
 }
 
-func isContains(_ number: Int) -> Bool {
-    var start = 0
-    var end = numbers.count - 1
-    while start <= end {
-        let half = (start + end) / 2
-        switch numbers[half] {
-        case ..<number:
-            start = half + 1
-        case number:
-            return true
-        default:
-            end = half - 1
-        }
-    }
-    return false
-}
+print(result)
