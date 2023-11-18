78c471c494efca4d37070bdaf03813c5275c1058
GUNDY <106914201+Gundy93@users.noreply.github.com>
2023-11-18 20:08:07 +0900

    Update 1920_수 찾기.swift

--- Gundy/BAEKJOON/1920_수 찾기.swift
+++ Gundy/BAEKJOON/1920_수 찾기.swift
@@ -1,17 +1,11 @@
 let _ = readLine()
-let list = readLine()!.split(separator: " ").map { Int($0)! }
-var logger = [Int: Int]()
-
-for number in list {
-    logger[number] = 1
-}
-
+let list = Set(readLine()!.split(separator: " ").map { Int($0)! })
 let _ = readLine()
 let numbers = readLine()!.split(separator: " ").map { Int($0)! }
 var result = String()
 
 for number in numbers {
-    result += "\(logger[number] ?? 0)\n"
+    result += "\(list.contains(number) ? 1 : 0)\n"
 }
 
 print(result)
