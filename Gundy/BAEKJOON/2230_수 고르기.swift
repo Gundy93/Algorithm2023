ebd46df08aa6024d0360d9be3f870ee6bca85836
GUNDY <106914201+Gundy93@users.noreply.github.com>
2023-11-18 20:23:13 +0900

    Update 2230_수 고르기.swift

--- Gundy/BAEKJOON/2230_수 고르기.swift
+++ Gundy/BAEKJOON/2230_수 고르기.swift
@@ -1,9 +1,61 @@
-var result = 2000000000
-let input = readLine()!.split(separator: " ").compactMap({ Int($0) })
-var numbers = [Int]()
+import Foundation
 
-for _ in 1...input[0] {
-    numbers.append(Int(readLine()!)!)
+final class FileIO {
+    private var buffer:[UInt8]
+    private var index: Int
+    
+    init(fileHandle: FileHandle = FileHandle.standardInput) {
+        buffer = Array(fileHandle.readDataToEndOfFile())+[UInt8(0)]
+        index = 0
+    }
+    
+    @inline(__always) private func read() -> UInt8 {
+        defer { index += 1 }
+        
+        return buffer.withUnsafeBufferPointer { $0[index] }
+    }
+    
+    @inline(__always) func readInt() -> Int {
+        var sum = 0
+        var now = read()
+        var isPositive = true
+        
+        while now == 10
+                || now == 32 { now = read() }
+        if now == 45{ isPositive.toggle(); now = read() }
+        while now >= 48, now <= 57 {
+            sum = sum * 10 + Int(now-48)
+            now = read()
+        }
+        
+        return sum * (isPositive ? 1:-1)
+    }
+    
+    @inline(__always) func readString() -> String {
+        var str = ""
+        var now = read()
+        
+        while now == 10
+                || now == 32 { now = read() }
+        
+        while now != 10
+                && now != 32 && now != 0 {
+            str += String(bytes: [now], encoding: .ascii)!
+            now = read()
+        }
+        
+        return str
+    }
+}
+
+let fileIO = FileIO()
+let count = fileIO.readInt()
+let minimum = fileIO.readInt()
+var numbers = [Int]()
+var result = Int.max
+
+for _ in 1...count {
+    numbers.append(fileIO.readInt())
 }
 
 numbers.sort()
@@ -14,10 +66,8 @@ var right = 0
 while right < numbers.count,
       left < numbers.count {
     let difference = numbers[right] - numbers[left]
-    if difference >= input[1] {
-        if difference < result {
-            result = difference
-        }
+    if difference >= minimum {
+        result = min(result, difference)
         left += 1
     } else {
         right += 1
