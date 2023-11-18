ecdbc6a727a269fba7d7ceea806385d07c430e85
GUNDY <106914201+Gundy93@users.noreply.github.com>
2023-11-18 11:04:36 +0900

    Update 2587_대표값2.swift

--- Gundy/BAEKJOON/2587_대표값2.swift
+++ Gundy/BAEKJOON/2587_대표값2.swift
@@ -1,11 +1,9 @@
-func solution() {
-    var numbers: [Int] = []
-    for _ in 1...5 {
-        let number: Int = Int(readLine()!)!
-        numbers.append(number)
-    }
-    print(numbers.reduce(0, +) / 5)
-    print(numbers.sorted()[2])
+var numbers = [Int]()
+
+for _ in 1...5 {
+    numbers.append(Int(readLine()!)!)
 }
 
-solution()
+numbers.sort()
+print(numbers.reduce(0, +) / 5)
+print(numbers[2])
