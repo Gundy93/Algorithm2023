22f81ca20f25081f6634c786ce6957b72c4c2915
GUNDY <106914201+Gundy93@users.noreply.github.com>
2023-11-21 16:31:05 +0900

    Create 11866_요세푸스 문제 0.swift

---
+++ Gundy/BAEKJOON/11866_요세푸스 문제 0.swift
@@ -0,0 +1,53 @@
+struct LinkedList<T> {
+    
+    final class Node {
+        
+        var value: T
+        var next: Node?
+        
+        init(value: T) {
+            self.value = value
+        }
+    }
+    
+    var head: Node?
+    var tail: Node?
+    
+    mutating func insert(_ element: T) {
+        let node = Node(value: element)
+        
+        guard let tail = tail else {
+            head = node
+            tail = node
+            return
+        }
+        
+        tail.next = node
+        self.tail = node
+    }
+}
+
+let input = readLine()!.split(separator: " ").compactMap { Int($0) }
+var list = LinkedList<Int>()
+
+for number in 1...input[0] {
+    list.insert(number)
+}
+
+list.tail?.next = list.head
+
+var result = [String]()
+var current = list.tail
+
+while result.count < input[0] {
+    for _ in stride(from: 1, to: input[1], by: 1) {
+        current = current?.next
+    }
+    
+    guard let next = current?.next else { break }
+    
+    result.append(String(next.value))
+    current?.next = next.next
+}
+
+print("<" + result.joined(separator: ", ") + ">")
