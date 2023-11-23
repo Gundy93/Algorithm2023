90bbb35b012aa5bc00ab78c7c235168425f4f25a
GUNDY <106914201+Gundy93@users.noreply.github.com>
2023-11-23 23:49:03 +0900

    Update [카카오 인턴] 수식 최대화.swift

--- Gundy/Programmers/LV. 2/[카카오 인턴] 수식 최대화.swift
+++ Gundy/Programmers/LV. 2/[카카오 인턴] 수식 최대화.swift
@@ -1,45 +1,63 @@
 func solution(_ expression:String) -> Int64 {
+    var expression = expression.map(String.init)
+    var stack = String()
     var elements = [String]()
-    var number = String()
-    for word in expression.map(String.init) {
-        switch word {
-        case "+", "-", "*":
-            elements += [number, word]
-            number = String()
-        default:
-            number += word
+    
+    for word in expression {
+        guard Int(word) != nil else {
+            elements.append(stack)
+            elements.append(word)
+            stack.removeAll()
+            
+            continue
         }
+        
+        stack += word
     }
-    elements.append(number)
-    var result = 0
-    for operators in [["+", "-", "*"], ["+", "*", "-"], ["-", "+", "*"], ["-", "*", "+"], ["*", "+", "-"], ["*", "-", "+"]] {
-        var expression = elements
-        for sign in operators {
-            var nextExpression = [String]()
-            var index = 0
-            while index < expression.count {
-                if expression[index] == sign {
-                    let lhs = Int(nextExpression.removeLast())!
-                    let rhs = Int(expression[index + 1])!
-                    switch sign {
-                    case "+":
-                        nextExpression.append(String(lhs + rhs))
-                    case "-":
-                        nextExpression.append(String(lhs - rhs))
-                    default:
-                        nextExpression.append(String(lhs * rhs))
-                    }
-                    index += 2
-                } else {
-                    nextExpression.append(expression[index])
-                    index += 1
-                }
+    
+    elements.append(stack)
+    
+    func calculate(_ elements: [String], _ sign: String) -> [String] {
+        var elements = elements
+        
+        switch sign {
+        case "+":
+            while let firstIndex = elements.firstIndex(of: sign) {
+                elements[firstIndex - 1] = String(Int(elements[firstIndex - 1])! + Int(elements[firstIndex + 1])!)
+                elements = Array(elements[0...firstIndex - 1]) + stride(from: firstIndex + 2, to: elements.count, by: 1).map { elements[$0] }
             }
-            expression = nextExpression
+        case "-":
+            while let firstIndex = elements.firstIndex(of: sign) {
+                elements[firstIndex - 1] = String(Int(elements[firstIndex - 1])! - Int(elements[firstIndex + 1])!)
+                elements = Array(elements[0...firstIndex - 1]) + stride(from: firstIndex + 2, to: elements.count, by: 1).map { elements[$0] }
+            }
+        default:
+            while let firstIndex = elements.firstIndex(of: sign) {
+                elements[firstIndex - 1] = String(Int(elements[firstIndex - 1])! * Int(elements[firstIndex + 1])!)
+                elements = Array(elements[0...firstIndex - 1]) + stride(from: firstIndex + 2, to: elements.count, by: 1).map { elements[$0] }
+            }
         }
-        if abs(Int(expression[0])!) > result {
-            result = abs(Int(expression[0])!)
+        
+        return elements
+    }
+    
+    var result = 0
+    
+    for first in ["+", "-", "*"] {
+        let firstResult = calculate(elements, first)
+        
+        for second in ["+", "-", "*"] {
+            guard first != second else { continue }
+            
+            let secondResult = calculate(firstResult, second)
+            
+            for third in ["+", "-", "*"] {
+                guard first != third, second != third else { continue }
+                
+                result = max(result, abs(Int(calculate(secondResult, third)[0])!))
+            }
         }
     }
+    
     return Int64(result)
 }
