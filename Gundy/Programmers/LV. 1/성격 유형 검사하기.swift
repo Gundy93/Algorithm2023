6dd0618eb90e9f7e5f454be644294015c6f6f0b5
GUNDY <106914201+Gundy93@users.noreply.github.com>
2023-11-23 22:02:48 +0900

    Update 성격 유형 검사하기.swift

--- Gundy/Programmers/LV. 1/성격 유형 검사하기.swift
+++ Gundy/Programmers/LV. 1/성격 유형 검사하기.swift
@@ -1,52 +1,20 @@
-var chart: [Int] = Array(repeating: 0, count: 4)
-
 func solution(_ survey:[String], _ choices:[Int]) -> String {
-    for (type, choice) in zip(survey, choices) {
-        makePoint(for: type, choice)
-    }
-    var result: String = ""
-    for (character, index) in zip(["RT", "FC", "MJ", "AN"], 0...3) {
-        switch chart[index] {
-        case 1...:
-            result += String(character.last!)
-        case ..<0:
-            result += String(character.first!)
+    var points = [String: Int]()
+    
+    for index in 0...survey.count - 1 {
+        var question = survey[index]
+        var choice = choices[index]
+        
+        switch question {
+        case "TR", "FC", "MJ", "NA":
+            choice = 8 - choice
+            question = String(question.reversed())
         default:
-            result += String(character.sorted().first!)
+            break
         }
+        
+        points[question, default: 0] += choice - 4
     }
-    return result
-}
-
-func makePoint(for type: String, _ choice: Int) {
-    var point: Int = abs(choice - 4)
-    var isPositive: Bool = choice > 4
-    var index: Int = 0
-    switch type {
-    case "FC", "CF":
-        if type == "CF" {
-            isPositive.toggle()
-        }
-        index = 1
-    case "MJ", "JM":
-        if type == "JM" {
-            isPositive.toggle()
-        }
-        index = 2
-    case "AN", "NA":
-        if type == "NA" {
-            isPositive.toggle()
-        }
-        index = 3
-    default:
-        if type == "TR" {
-            isPositive.toggle()
-        }
-        break
-    }
-    if isPositive {
-        chart[index] += point
-    } else {
-        chart[index] -= point
-    }
+    
+    return ["RT", "CF", "JM", "AN"].map { String(points[$0, default: 0] == 0 ? $0.sorted().first! : points[$0]! < 0 ? $0.first! : $0.last!) }.joined()
 }
