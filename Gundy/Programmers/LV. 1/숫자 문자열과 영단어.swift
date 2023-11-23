0c21c037818bab7eefbe111d22aa7a85556c3e81
GUNDY <106914201+Gundy93@users.noreply.github.com>
2023-11-23 22:06:16 +0900

    Update 숫자 문자열과 영단어.swift

--- Gundy/Programmers/LV. 1/숫자 문자열과 영단어.swift
+++ Gundy/Programmers/LV. 1/숫자 문자열과 영단어.swift
@@ -1,45 +1,9 @@
-extension String {
-    func toInt() -> Int? {
-        switch self {
-        case "zero":
-            return 0
-        case "one":
-            return 1
-        case "two":
-            return 2
-        case "three":
-            return 3
-        case "four":
-            return 4
-        case "five":
-            return 5
-        case "six":
-            return 6
-        case "seven":
-            return 7
-        case "eight":
-            return 8
-        case "nine":
-            return 9
-        default:
-            return nil
-        }
-    }
-}
-
 func solution(_ s:String) -> Int {
-    var result: String = ""
-    var input: [String] = s.reversed().map({ String($0) })
-    var temporaryValue: String = ""
-    while input.isEmpty == false {
-        temporaryValue += input.removeLast()
-        if let number = temporaryValue.toInt() {
-            result += number.description
-            temporaryValue = ""
-        } else if let number = Int(temporaryValue) {
-            result += number.description
-            temporaryValue = ""
-        }
+    var result = s
+    
+    for (word, number) in zip(["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"], 0...9) {
+        result = result.replacingOccurrences(of: word, with: String(number))
     }
+    
     return Int(result)!
 }
