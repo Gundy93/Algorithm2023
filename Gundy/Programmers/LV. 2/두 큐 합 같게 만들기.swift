cd366ccc7e252dc3315f4fa363749ee341ae1f45
GUNDY <106914201+Gundy93@users.noreply.github.com>
2023-11-23 22:37:33 +0900

    Update 두 큐 합 같게 만들기.swift

--- Gundy/Programmers/LV. 2/두 큐 합 같게 만들기.swift
+++ Gundy/Programmers/LV. 2/두 큐 합 같게 만들기.swift
@@ -1,53 +1,92 @@
-struct Queue {
-    private var output: [Int] = []
-    private var input: [Int]
-
-    init(_ values: [Int]) {
-        input = values
-    }
-
-    mutating func enqueue(_ newElement: Int) {
-        input.append(newElement)
-    }
-
-    mutating func dequeue() -> Int? {
-        if output.isEmpty {
-            if input.isEmpty {
-                return nil
-            }
-            output = input.reversed()
-            input.removeAll()
+struct Queue<T> {
+    
+    final class Node {
+        
+        let value: T
+        var next: Node?
+        
+        init(value: T) {
+            self.value = value
         }
-        return output.popLast()
+    }
+    
+    private var head: Node?
+    private var tail: Node?
+    var first: T? {
+        return head?.value
+    }
+    var last: T? {
+        return tail?.value
+    }
+    
+    mutating func enqueue(_ newElement: T) {
+        let node = Node(value: newElement)
+        
+        guard let lastNode = tail else {
+            head = node
+            tail = node
+            
+            return
+        }
+        
+        lastNode.next = node
+        tail = node
+    }
+    
+    mutating func dequeue() -> T? {
+        let element = first
+        
+        if head?.next == nil {
+            tail = nil
+        }
+        
+        head = head?.next
+        
+        return element
     }
 }
 
 func solution(_ queue1:[Int], _ queue2:[Int]) -> Int {
-    var result = 0
-    var leftQueue = Queue(queue1)
-    var leftValue = queue1.reduce(0, +)
-    var rightQueue = Queue(queue2)
-    var rightValue = queue2.reduce(0, +)
-    guard (leftValue + rightValue) % 2 == 0 else {
-        return -1
+    var left = 0
+    var leftQueue = Queue<Int>()
+    
+    for number in queue1 {
+        left += number
+        leftQueue.enqueue(number)
     }
-    let target = (leftValue + rightValue) / 2
-    for _ in 1...queue1.count * 4 {
-        guard leftValue != rightValue else {
-            break
-        }
-        if leftValue < rightValue {
-            let number = rightQueue.dequeue()!
-            leftQueue.enqueue(number)
-            leftValue += number
-            rightValue -= number
-        } else {
-            let number = leftQueue.dequeue()!
+    
+    var right = 0
+    var rightQueue = Queue<Int>()
+    
+    for number in queue2 {
+        right += number
+        rightQueue.enqueue(number)
+    }
+    
+    let total = left + right
+    
+    guard total % 2 == 0 else { return -1 }
+    
+    var count = 0
+    
+    while left != right,
+          count < (queue1.count + queue2.count) * 2 {
+        if left > right {
+            guard let number = leftQueue.dequeue() else { return -1 }
+            
+            left -= number
+            right += number
             rightQueue.enqueue(number)
-            rightValue += number
-            leftValue -= number
+        } else {
+            guard let number = rightQueue.dequeue() else { return -1 }
+            
+            right -= number
+            left += number
+            leftQueue.enqueue(number)
         }
-        result += 1
+        
+        count += 1
     }
-    return leftValue == target ? result : -1
+    
+    return left == right ? count : -1
 }
