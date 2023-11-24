6b9643b10b0a5103f7ffe235fdfb90745918ab6c
GUNDY <106914201+Gundy93@users.noreply.github.com>
2023-11-24 22:13:11 +0900

    Update [카카오 인턴] 보석 쇼핑.swift

--- Gundy/Programmers/LV. 3/[카카오 인턴] 보석 쇼핑.swift
+++ Gundy/Programmers/LV. 3/[카카오 인턴] 보석 쇼핑.swift
@@ -1,31 +1,47 @@
 func solution(_ gems:[String]) -> [Int] {
-    var result = [1, gems.count]
-    let gemKind = Set(gems).count
+    var numberOfGemTypes = Set(gems)
     var start = 0
     var end = 0
-    var list = Set([gems[0]])
-    guard list.count < gemKind else { return [1, 1] }
-    var count = [String: Int]()
-    count[gems[0]] = 1
-    while end < gems.count {
-        if list.count == gemKind {
+    var counter = [String: Int]()
+    
+    counter[gems[0]] = 1
+    
+    var result = [1, gems.count]
+    
+    while end < gems.count - 1,
+          start <= end {
+        if counter.keys.count == numberOfGemTypes.count {
             if result[1] - result[0] > end - start {
                 result = [start + 1, end + 1]
             }
-            count[gems[start]]! -= 1
-            if count[gems[start]] == 0 {
-                list.remove(gems[start])
+            
+            counter[gems[start], default: 0] -= 1
+            
+            if counter[gems[start]] == 0 {
+                counter.removeValue(forKey: gems[start])
             }
+            
             start += 1
         } else {
             end += 1
-            if end < gems.count {
-                count[gems[end], default: 0] += 1
-                if count[gems[end]] == 1 {
-                    list.insert(gems[end])
-                }
-            }
+            counter[gems[end], default: 0] += 1
         }
     }
+    
+    while start <= end,
+          counter.keys.count == numberOfGemTypes.count {
+        if result[1] - result[0] > end - start {
+            result = [start + 1, end + 1]
+        }
+        
+        counter[gems[start], default: 0] -= 1
+        
+        if counter[gems[start]] == 0 {
+            counter.removeValue(forKey: gems[start])
+        }
+        
+        start += 1
+    }
+    
     return result
 }
