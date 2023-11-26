75b5bf1f37d7dcf190f4acf7657a133f9e7a132d
GUNDY <106914201+Gundy93@users.noreply.github.com>
2023-11-26 13:54:43 +0900

    Update 1463_1로 만들기.swift

--- Gundy/BAEKJOON/1463_1로 만들기.swift
+++ Gundy/BAEKJOON/1463_1로 만들기.swift
@@ -1,15 +1,15 @@
 let target = Int(readLine()!)!
-var dp = Array(repeating: 0, count: target + 1)
+var dp = Array(repeating: Int.max, count: target + 1)
 
 dp[1] = 0
 
-for number in stride(from: 2, through: target, by: 1) {
-    if number % 3 == 0 && number % 2 == 0 {
-        dp[number] = min(dp[number - 1], min(dp[number / 3], dp[number / 2])) + 1
+for number in stride(from: 4, through: target, by: 1) {
+    if number % 6 == 0 {
+        dp[number] = min(dp[number - 1], min(dp[number / 2], dp[number / 3])) + 1
     } else if number % 3 == 0 {
         dp[number] = min(dp[number - 1], dp[number / 3]) + 1
     } else if number % 2 == 0 {
-        dp[number] = min(dp[number - 1],  dp[number / 2]) + 1
+        dp[number] = min(dp[number - 1], dp[number / 2]) + 1
     } else {
         dp[number] = dp[number - 1] + 1
     }
