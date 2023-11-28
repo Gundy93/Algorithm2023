614b7f9a7cdfd8860ce49bfc8081bddf77eabc77
GUNDY <106914201+Gundy93@users.noreply.github.com>
2023-11-28 19:02:29 +0900

    Create 2559_수열.swift

---
+++ Gundy/BAEKJOON/2559_수열.swift
@@ -0,0 +1,17 @@
+let input = readLine()!.split(separator: " ").map { Int($0)! }
+let temperature = readLine()!.split(separator: " ").map { Int($0)! }
+var prefixSum = Array(repeating: 0, count: input[0] + 1)
+
+prefixSum[1] = temperature[0]
+
+for index in stride(from: 1, to: input[0], by: 1) {
+    prefixSum[index + 1] = prefixSum[index] + temperature[index]
+}
+
+var dp = Array(repeating: 0, count: input[0] + 1 - input[1])
+
+for index in stride(from: 0, to: input[0] + 1 - input[1], by: 1) {
+    dp[index] = prefixSum[index + input[1]] - prefixSum[index]
+}
+
+print(dp.max()!)
