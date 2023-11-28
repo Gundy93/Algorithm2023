e089dea2b9eb47c26ae41b2202789fc0d91d04aa
GUNDY <106914201+Gundy93@users.noreply.github.com>
2023-11-28 19:03:26 +0900

    Update 11659_구간 합 구하기 4.swift

--- Gundy/BAEKJOON/11659_구간 합 구하기 4.swift
+++ Gundy/BAEKJOON/11659_구간 합 구하기 4.swift
@@ -2,10 +2,7 @@ let input = readLine()!.split(separator: " ").map { Int($0)! }
 let numbers = readLine()!.split(separator: " ").map { Int($0)! }
 var dp = Array(repeating: 0, count: input[0] + 1)
 
-dp[0] = 0
-dp[1] = numbers[0]
-
-for index in stride(from: 1, to: input[0], by: 1) {
+for index in stride(from: 0, to: input[0], by: 1) {
     dp[index + 1] = dp[index] + numbers[index]
 }
 
