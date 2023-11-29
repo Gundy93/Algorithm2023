23252e9a0592d254465f1db72e071726b97f03c4
GUNDY <106914201+Gundy93@users.noreply.github.com>
2023-11-29 13:16:44 +0900

    Update 1931_회의실 배정.swift

--- Gundy/BAEKJOON/1931_회의실 배정.swift
+++ Gundy/BAEKJOON/1931_회의실 배정.swift
@@ -2,23 +2,21 @@ let count = Int(readLine()!)!
 var times = [(start: Int, end: Int)]()
 
 for _ in 1...count {
-    let input = readLine()!.split(separator: " ").map { Int($0)! }
+    let meeting = readLine()!.split(separator: " ").map { Int($0)! }
     
-    times.append((input[0], input[1]))
+    times.append((meeting[0], meeting[1]))
 }
 
 times.sort { $0.end == $1.end ? $0.start < $1.start : $0.end < $1.end }
 
-var result = 0
-var last = (start: -1, end: -1)
+var result = 1
+var last = times[0].end
 
-for index in 0...count - 1 {
-    let current = times[index]
-    
-    guard last.end <= current.start else { continue }
+for index in stride(from: 1, to: count, by: 1) {
+    guard times[index].start >= last else { continue }
     
     result += 1
-    last = current
+    last = times[index].end
 }
 
 print(result)
