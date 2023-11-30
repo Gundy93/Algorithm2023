7a7777d9764819bc3cc114e323bf9044bfd85765
GUNDY <106914201+Gundy93@users.noreply.github.com>
2023-11-30 13:25:58 +0900

    Update 2630_색종이 만들기.swift

--- Gundy/BAEKJOON/2630_색종이 만들기.swift
+++ Gundy/BAEKJOON/2630_색종이 만들기.swift
@@ -1,34 +1,33 @@
-let length: Int = Int(readLine()!)!
-var board: [[String]] = []
-for _ in 1...length {
-    board.append(readLine()!.split(separator: " ").map({ String($0) }))
-}
-let result = recursion(board.count / 2, originPoint: (0, 0))
-print(result.zero)
-print(result.one)
+let length = Int(readLine()!)!
+var paper = [[String]]()
 
-func recursion(_ term: Int, originPoint: (row: Int, column: Int)) -> (one: Int, zero: Int) {
-    if term == 0 {
-        switch board[originPoint.row][originPoint.column] {
-        case "1":
-            return (1, 0)
-        default:
-            return (0, 1)
-        }
-    }
-    let origin = recursion(term / 2, originPoint: originPoint)
-    var isEqual: Bool = origin.one + origin.zero == 1 ? true : false
-    var one: Int = origin.one
-    var zero: Int = origin.zero
-    for (row, column) in zip([0, term, term],
-                             [term, 0, term]) {
-        let temporary = recursion(term / 2, originPoint: (originPoint.row + row, originPoint.column + column))
-        if isEqual,
-           origin != temporary {
-            isEqual = false
-        }
-        one += temporary.one
-        zero += temporary.zero
-    }
-    return isEqual ? origin : (one, zero)
+for _ in 1...length {
+    paper.append(readLine()!.split(separator: " ").map(String.init))
 }
+
+func countPapers(_ row: Int, _ column: Int, _ length: Int) -> (zero: Int, one: Int) {
+    guard length > 1 else { return paper[row][column] == "0" ? (1,0) : (0,1) }
+    
+    let first = countPapers(row, column, length / 2)
+    var isWholePaper = first.zero + first.one == 1
+    var zero = first.zero
+    var one = first.one
+    
+    for (row, column) in zip([row + length / 2, row, row + length / 2],
+                             [column, column + length / 2, column + length / 2]) {
+        let next = countPapers(row, column, length / 2)
+        
+        if isWholePaper {
+            isWholePaper = first == next
+        }
+        
+        zero += next.zero
+        one += next.one
+    }
+    
+    return isWholePaper ? first : (zero, one)
+}
+
+let result = countPapers(0, 0, length)
+
+print("\(result.zero)\n\(result.one)")
