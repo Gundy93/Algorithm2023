7aa6e85157293b88f24e1acfa14dd09949c98efb
GUNDY <106914201+Gundy93@users.noreply.github.com>
2023-11-30 13:37:08 +0900

    Update 1992_쿼드트리.swift

--- Gundy/BAEKJOON/1992_쿼드트리.swift
+++ Gundy/BAEKJOON/1992_쿼드트리.swift
@@ -1,22 +1,30 @@
-var image = [[String]]()
+let length = Int(readLine()!)!
+var screen = [[String]]()
 
-for _ in 1...Int(readLine()!)! {
-    image.append(readLine()!.map({ String($0) }))
+for _ in 1...length {
+    screen.append(readLine()!.map(String.init))
 }
 
-print(recursion(origin: (0, 0), size: image.count))
 
-func recursion(origin point: (x: Int, y: Int), size: Int) -> String {
-    guard size > 1 else { return image[point.x][point.y] }
-
-    let xPoints = [point.x, point.x, point.x + size / 2, point.x + size / 2]
-    let yPoints = [point.y, point.y + size / 2, point.y, point.y + size / 2]
-    let result = zip(xPoints, yPoints).map({ recursion(origin: ($0, $1), size: size / 2) })
-
-    if result[0].count == 1,
-       result.filter({ result[0] == $0 }).count == 4 {
-        return result[0]
-    } else {
-        return "(" + result.joined() + ")"
+func quadTree(_ row: Int, _ column: Int, _ length: Int) -> String {
+    guard length > 1 else { return screen[row][column] }
+    
+    let first = quadTree(row, column, length / 2)
+    var isWhole = first.count == 1
+    var result = first
+    
+    for (row, column) in zip([row, row + length / 2, row + length / 2],
+                             [column + length / 2, column, column + length / 2]) {
+        let next = quadTree(row, column, length / 2)
+        
+        if isWhole {
+            isWhole = first == next
+        }
+        
+        result += next
     }
+    
+    return isWhole ? first : "(\(result))"
 }
+
+print(quadTree(0, 0, length))
