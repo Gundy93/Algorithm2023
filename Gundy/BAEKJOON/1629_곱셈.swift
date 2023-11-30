27b2b278e6269c4ddd2ac1fc6cd56a85643c6aa4
GUNDY <106914201+Gundy93@users.noreply.github.com>
2023-11-30 15:47:18 +0900

    Update 1629_곱셈.swift

--- Gundy/BAEKJOON/1629_곱셈.swift
+++ Gundy/BAEKJOON/1629_곱셈.swift
@@ -1,19 +1,15 @@
-let input = readLine()!.split(separator: " ").map { Int($0)! }
+let numbers = readLine()!.split(separator: " ").map { Int($0)! }
 
-func recursion(_ count: Int) -> Int {
-    switch count {
-    case 0:
-        return 1
-    case 1:
-        return input[0] % input[2]
-    default:
-        let next = recursion(count / 2) % input[2]
-        if count % 2 == 0 {
-            return next * next % input[2]
-        } else {
-            return next * next % input[2] * input[0] % input[2]
-        }
+func calculate(_ count: Int) -> Int {
+    guard count > 1 else { return numbers[0] % numbers[2] }
+    
+    let next = calculate(count / 2)
+    
+    if count % 2 == 1 {
+        return next * next % numbers[2] * numbers[0] % numbers[2]
+    } else {
+        return next * next % numbers[2]
     }
 }
 
-print(recursion(input[1]))
+print(calculate(numbers[1]))
