b2183c58a54f33234cb9200c6b133d1539461e8e
GUNDY <106914201+Gundy93@users.noreply.github.com>
2023-12-01 14:28:56 +0900

    Update 5430_AC.swift

--- Gundy/BAEKJOON/5430_AC.swift
+++ Gundy/BAEKJOON/5430_AC.swift
@@ -1,46 +1,136 @@
-import Foundation
+struct Deque<T> {
+    
+    final class Node {
+        
+        let value: T
+        var previous: Node?
+        var next: Node?
+        
+        init(value: T) {
+            self.value = value
+        }
+    }
+    
+    private var head: Node?
+    private var tail: Node?
+    var first: T? {
+        return head?.value
+    }
+    var last: T? {
+        return tail?.value
+    }
+    
+    mutating func pushFirst(_ newElement: T) {
+        let node = Node(value: newElement)
+        
+        guard let first = head else {
+            head = node
+            tail = node
+            
+            return
+        }
+        
+        first.previous = node
+        node.next = first
+        head = node
+    }
+    
+    mutating func pushLast(_ newElement: T) {
+        let node = Node(value: newElement)
+        
+        guard let last = tail else {
+            head = node
+            tail = node
+            
+            return
+        }
+        
+        last.next = node
+        node.previous = last
+        tail = node
+    }
+    
+    mutating func popFirst() -> T? {
+        let element = first
+        
+        guard head != nil else { return element }
+        
+        if head === tail {
+            head = nil
+            tail = nil
+        } else {
+            head = head?.next
+        }
+        
+        return element
+    }
+    
+    mutating func popLast() -> T? {
+        let element = last
+        
+        guard tail != nil else { return element }
+        
+        if head === tail {
+            head = nil
+            tail = nil
+        } else {
+            tail = tail?.previous
+        }
+        
+        return element
+    }
+}
 
 let count = Int(readLine()!)!
+var result = String()
 
-for _ in 1...count {
+loop: for _ in 1...count {
     let commands = readLine()!
-    var count = Int(readLine()!)!
-    var numbers = readLine()!.components(separatedBy: ["[", ",", "]"]).compactMap(Int.init)
-    var left = 0
-    var right = count - 1
-    var isForward = true
-    var isValid = true
+    let _ = readLine()
+    var input = readLine()!
+    
+    input.removeFirst()
+    input.removeLast()
+    
+    var deque = Deque<String>()
+    
+    for number in input.split(separator: ",").map(String.init) {
+        deque.pushLast(number)
+    }
+    
+    var isReversed = false
     
     for command in commands {
         switch command {
         case "R":
-            isForward.toggle()
+            isReversed.toggle()
         default:
-            guard count > 0 else {
-                isValid = false
-                
-                break
+            guard deque.first != nil else {
+                result += "error\n"
+                continue loop
             }
             
-            if isForward {
-                left += 1
-            } else {
-                right -= 1
-            }
-            
-            count -= 1
+            let _ = isReversed ? deque.popLast() : deque.popFirst()
         }
     }
     
-    if isValid {
-        var result = stride(from: left, through: right, by: 1).map { String(numbers[$0]) }
-        
-        if isForward == false {
-            result = result.reversed()
+    var numbers = "["
+    
+    if isReversed {
+        while let number = deque.popLast() {
+            numbers += number + ","
         }
-        
-        print("[" + result.joined(separator: ",") + "]")
     } else {
-        print("error")
+        while let number = deque.popFirst() {
+            numbers += number + ","
+        }
     }
+    
+    if numbers.last == "," {
+        numbers.removeLast()
+    }
+    
+    result += numbers + "]\n"
 }
+
+print(result)
