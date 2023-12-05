4256430027f55b2d77ea736273fe34cc0483946c
GUNDY <106914201+Gundy93@users.noreply.github.com>
2023-12-05 14:01:27 +0900

    Update 3986_좋은 단어.swift

--- Gundy/BAEKJOON/3986_좋은 단어.swift
+++ Gundy/BAEKJOON/3986_좋은 단어.swift
@@ -1,21 +1,21 @@
-func solution() {
-    let count: Int = Int(readLine()!)!
-    var result: Int = 0
-    for _ in 1...count {
-        var text: String = readLine()!
-        var stack: [Character] = []
-        for character in text {
-            if stack.last == character {
-                stack.removeLast()
-            } else {
-                stack.append(character)
-            }
-        }
-        if stack.isEmpty {
-            result += 1
+let count = Int(readLine()!)!
+var result = 0
+
+for _ in 1...count {
+    let words = readLine()!
+    var stack = [Character]()
+    
+    for word in words {
+        if stack.last == word {
+            stack.removeLast()
+        } else {
+            stack.append(word)
         }
     }
-    print(result)
+    
+    if stack.isEmpty {
+        result += 1
+    }
 }
 
-solution()
+print(result)
