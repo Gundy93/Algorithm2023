db0430554f85cfa185be8d029e213c5abdaf5e72
GUNDY <106914201+Gundy93@users.noreply.github.com>
2023-12-05 12:48:41 +0900

    Update 11286_절댓값 힙.swift

--- Gundy/BAEKJOON/11286_절댓값 힙.swift
+++ Gundy/BAEKJOON/11286_절댓값 힙.swift
@@ -1,92 +1,72 @@
-struct Heap<Element> {
-    private var container: [Element]
-    var isEmpty: Bool {
-        return container.isEmpty
-    }
-    var count: Int {
-        return container.count
-    }
-    private var condition: (Element, Element) -> Bool
-
-    init(_ elements: [Element] = [], condition: @escaping (Element, Element) -> Bool) {
-        container = elements
+struct Heap<T> {
+    private(set) var box: [T] = []
+    private var condition: (T, T) -> Bool
+    var isEmpty: Bool { return box.isEmpty }
+    var count: Int { return box.count }
+    
+    init(_ condition: @escaping (T, T) -> Bool) {
         self.condition = condition
     }
-
-    mutating func insert(_ newElement: Element) {
-        container.append(newElement)
-
+    
+    private func getSuperIndex(of index: Int) -> Int {
+        return (index - 1) / 2
+    }
+    
+    private func getLeftIndex(of index: Int) -> Int {
+        return index * 2 + 1
+    }
+    
+    private func getRightIndex(of index: Int) -> Int {
+        return index * 2 + 2
+    }
+    
+    mutating func insert(_ newElement: T) {
+        box.append(newElement)
+        guard count > 1 else { return }
         var index = count - 1
-        var parentIndex = getParentIndex(of: index)
-
-        while index > 0,
-              condition(container[index], container[parentIndex]) {
-            container.swapAt(index, parentIndex)
-            index = parentIndex
-            parentIndex = getParentIndex(of: index)
+        while index > 0 {
+            let superIndex = getSuperIndex(of: index)
+            guard condition(box[index], box[superIndex]) else { return }
+            box.swapAt(superIndex, index)
+            index = superIndex
         }
     }
-
-    mutating func remove() -> Element? {
-        guard isEmpty == false else {
-            return nil
-        }
-
-        container.swapAt(0, container.count - 1)
-
-        let element = container.removeLast()
+    
+    mutating func removeFirst() -> T? {
+        guard count > 1 else { return box.popLast() }
+        box.swapAt(0, count - 1)
+        let element = box.removeLast()
         var index = 0
-
-        while index < count {
-            let leftChildIndex = getLeftChildIndex(of: index)
-            let rightChildIndex = getRightChildIndex(of: index)
-            var parentIndex = index
-
-            if leftChildIndex < count,
-               condition(container[leftChildIndex], container[parentIndex]) {
-                parentIndex = leftChildIndex
+        while index < count - 1 {
+            var superIndex = index
+            let leftIndex = getLeftIndex(of: index)
+            let rightIndex = getRightIndex(of: index)
+            if leftIndex < count, condition(box[leftIndex], box[superIndex]) {
+                superIndex = leftIndex
             }
-
-            if rightChildIndex < count,
-               condition(container[rightChildIndex], container[parentIndex]) {
-                parentIndex = rightChildIndex
+            if rightIndex < count, condition(box[rightIndex], box[superIndex]) {
+                superIndex = rightIndex
             }
-
-            guard index != parentIndex else {
-                break
-            }
-
-            container.swapAt(index, parentIndex)
-            index = parentIndex
+            guard index != superIndex else { break }
+            box.swapAt(index, superIndex)
+            index = superIndex
         }
         return element
     }
-
-    private func getParentIndex(of index: Int) -> Int {
-        return (index - 1) / 2
-    }
-
-    private func getLeftChildIndex(of index: Int) -> Int {
-        return index * 2 + 1
-    }
-
-    private func getRightChildIndex(of index: Int) -> Int {
-        return index * 2 + 2
-    }
 }
 
-var minimumHeap = Heap<(Int, Int)>(condition: <)
+let count = Int(readLine()!)!
+var absHeap = Heap<Int>() { abs($0) == abs($1) ? $0 < $1 : abs($0) < abs($1) }
+var result = String()
 
-for _ in 1...Int(readLine()!)! {
+for _ in 1...count {
     let number = Int(readLine()!)!
-    switch number {
-    case 0:
-        guard let minimumNumber = minimumHeap.remove() else {
-            print(0)
-            continue
-        }
-        print(minimumNumber.1)
-    default:
-        minimumHeap.insert((abs(number), number))
+    
+    if number == 0 {
+        result += "\(absHeap.removeFirst() ?? 0)\n"
+    } else {
+        absHeap.insert(number)
     }
 }
+
+print(result)
