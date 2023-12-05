8576c9c1c788eed2d84f51636f869d71e702e2b2
GUNDY <106914201+Gundy93@users.noreply.github.com>
2023-12-05 12:44:00 +0900

    Create 1927_최소 힙.swift

---
+++ Gundy/BAEKJOON/1927_최소 힙.swift
@@ -0,0 +1,72 @@
+struct Heap<T> {
+    private(set) var box: [T] = []
+    private var condition: (T, T) -> Bool
+    var isEmpty: Bool { return box.isEmpty }
+    var count: Int { return box.count }
+    
+    init(_ condition: @escaping (T, T) -> Bool) {
+        self.condition = condition
+    }
+    
+    private func getSuperIndex(of index: Int) -> Int {
+        return (index - 1) / 2
+    }
+    
+    private func getLeftIndex(of index: Int) -> Int {
+        return index * 2 + 1
+    }
+    
+    private func getRightIndex(of index: Int) -> Int {
+        return index * 2 + 2
+    }
+    
+    mutating func insert(_ newElement: T) {
+        box.append(newElement)
+        guard count > 1 else { return }
+        var index = count - 1
+        while index > 0 {
+            let superIndex = getSuperIndex(of: index)
+            guard condition(box[index], box[superIndex]) else { return }
+            box.swapAt(superIndex, index)
+            index = superIndex
+        }
+    }
+    
+    mutating func removeFirst() -> T? {
+        guard count > 1 else { return box.popLast() }
+        box.swapAt(0, count - 1)
+        let element = box.removeLast()
+        var index = 0
+        while index < count - 1 {
+            var superIndex = index
+            let leftIndex = getLeftIndex(of: index)
+            let rightIndex = getRightIndex(of: index)
+            if leftIndex < count, condition(box[leftIndex], box[superIndex]) {
+                superIndex = leftIndex
+            }
+            if rightIndex < count, condition(box[rightIndex], box[superIndex]) {
+                superIndex = rightIndex
+            }
+            guard index != superIndex else { break }
+            box.swapAt(index, superIndex)
+            index = superIndex
+        }
+        return element
+    }
+}
+
+let count = Int(readLine()!)!
+var maxHeap = Heap<Int>(<)
+var result = String()
+
+for _ in 1...count {
+    let number = Int(readLine()!)!
+    
+    if number == 0 {
+        result += "\(maxHeap.removeFirst() ?? 0)\n"
+    } else {
+        maxHeap.insert(number)
+    }
+}
+
+print(result)
