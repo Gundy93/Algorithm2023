8736efba2fbd708065b684be4fa79fc689334ed8
GUNDY <106914201+Gundy93@users.noreply.github.com>
2023-12-08 13:33:00 +0900

    Update 2504_괄호의 값.swift

--- Gundy/BAEKJOON/2504_괄호의 값.swift
+++ Gundy/BAEKJOON/2504_괄호의 값.swift
@@ -1,72 +1,39 @@
-func solution() {
-    let input: String = readLine()!
-    var characterStack: [Character] = []
-    for character in input {
-        if character == ")",
-           characterStack.last == "(" {
-            characterStack.removeLast()
-            characterStack.append("2")
-        } else if character == "]",
-           characterStack.last == "[" {
-            characterStack.removeLast()
-            characterStack.append("3")
-        } else {
-            characterStack.append(character)
-        }
-    }
-    let texts: [String] = characterStack.map({ String($0) })
-    var stack: [String] = []
-    for text in texts {
-        switch text {
-        case ")":
-            guard stack.isEmpty == false else {
-                print(0)
-                return
-            }
-            var number: Int = 0
-            while let lastNumber = Int(stack.last!) {
-                number += lastNumber
-                stack.removeLast()
-                if stack.isEmpty {
-                    print(0)
-                    return
+let input = readLine()!.map(String.init)
+var stack = [String]()
+var isValid = input.count % 2 == 0
+
+if isValid {
+    loop: for word in input {
+        switch word {
+        case "(", "[":
+            stack.append(word)
+        default:
+            var inner = 0
+            let target = word == ")" ? "(" : "["
+            let value = word == ")" ? 2 : 3
+            
+            while let last = stack.last,
+            last != target {
+                guard let number = Int(last) else {
+                    isValid = false
+                    
+                    break loop
                 }
+                
+                inner += number
+                stack.removeLast()
             }
             
-            guard stack.removeLast() == "(" else {
-                print(0)
-                return
+            guard stack.last == target else {
+                isValid = false
+                
+                break loop
             }
-            stack.append("\(number * 2)")
-        case "]":
-            guard stack.isEmpty == false else {
-                print(0)
-                return
-            }
-            var number: Int = 0
-            while let lastNumber = Int(stack.last!) {
-                number += lastNumber
-                stack.removeLast()
-                if stack.isEmpty {
-                    print(0)
-                    return
-                }
-            }
-            guard stack.removeLast() == "[" else {
-                print(0)
-                return
-            }
-            stack.append("\(number * 3)")
-        default:
-            stack.append(text)
+            
+            stack.removeLast()
+            stack.append(String(inner == 0 ? value : inner * value))
         }
-    }
-    let result: [Int] = stack.compactMap({ Int($0) })
-    if stack.count == result.count {
-        print(result.reduce(0, +))
-    } else {
-        print(0)
     }
 }
 
-solution()
+print(isValid ? stack.compactMap(Int.init).reduce(0, +) : 0)
