c94fb3198678bb8c3922b4cdd1bd0efbb1130978
GUNDY <106914201+Gundy93@users.noreply.github.com>
2023-12-15 14:13:17 +0900

    Update 7562_나이트의 이동.swift

--- Gundy/BAEKJOON/7562_나이트의 이동.swift
+++ Gundy/BAEKJOON/7562_나이트의 이동.swift
@@ -1,64 +1,84 @@
-extension Array {
-    subscript(safe index: Int) -> Element? {
-        guard self.count > index && index >= 0 else {
-            return nil
-        }
-        return self[index]
-    }
-}
-
 struct Queue<T> {
-    private var left: [T] = []
-    private var right: [T] = []
-    var isEmpty: Bool {
-        return left.isEmpty && right.isEmpty
+    
+    final class Node {
+        
+        let value: T
+        var next: Node?
+        
+        init(value: T) {
+            self.value = value
+        }
     }
     
-    mutating func enqueue(_ value: T) {
-        right.append(value)
+    private var head: Node?
+    private var tail: Node?
+    var first: T? {
+        return head?.value
+    }
+    var last: T? {
+        return tail?.value
+    }
+    var isEmpty: Bool {
+        return head == nil
+    }
+    
+    mutating func enqueue(_ newElement: T) {
+        let node = Node(value: newElement)
+        
+        guard let lastNode = tail else {
+            head = node
+            tail = node
+            
+            return
+        }
+        
+        lastNode.next = node
+        tail = node
     }
     
     mutating func dequeue() -> T? {
-        if left.isEmpty {
-            left = right.reversed()
-            right = []
+        let element = first
+        
+        if head?.next == nil {
+            tail = nil
         }
-        return left.popLast()
+        
+        head = head?.next
+        
+        return element
     }
 }
 
-func solution(length: Int, now: [Int], target: [Int]) {
-    let row: [Int] = Array(repeating: -1, count: length)
-    var board: [[Int]] = Array(repeating: row, count: length)
-    board[now.first!][now.last!] = 0
-    var visited: [[Bool]] = Array(repeating: Array(repeating: false, count: length), count: length)
-    var needVisit: Queue<[Int]> = Queue()
-    needVisit.enqueue([now.first!, now.last!])
-    let axisX: [Int] = [-2, -1, 1, 2, 2, 1, -1, -2]
-    let axisY: [Int] = [1, 2, 2, 1, -1, -2, -2, -1]
-    while needVisit.isEmpty == false {
-        let current: [Int] = needVisit.dequeue()!
-        let x: Int = current[0]
-        let y: Int = current[1]
-        guard visited[x][y] == false else { continue }
-        if current == target {
-            print(board[x][y])
-            return
-        }
-        visited[x][y] = true
-        for (deltaX, deltaY) in zip(axisX, axisY) {
-            if board[safe: x + deltaX]?[safe: y + deltaY] == -1 {
-                board[x + deltaX][y + deltaY] = board[x][y] + 1
-                needVisit.enqueue([x + deltaX, y + deltaY])
-            }
-        }
-    }
-}
+let count = Int(readLine()!)!
+var result = String()
 
-let count: Int = Int(readLine()!)!
 for _ in 1...count {
-    let length: Int = Int(readLine()!)!
-    let now: [Int] = readLine()!.split(separator: " ").compactMap({ Int($0) })
-    let target: [Int] = readLine()!.split(separator: " ").compactMap({ Int($0) })
-    solution(length: length, now: now, target: target)
+    let length = Int(readLine()!)!
+    let now = readLine()!.split(separator: " ").map { Int($0)! }
+    let target = readLine()!.split(separator: " ").map { Int($0)! }
+    var board = Array(repeating: Array(repeating: 0, count: length), count: length)
+    var queue = Queue<(row: Int, column: Int)>()
+    
+    queue.enqueue((now[0], now[1]))
+    
+    while let (row, column) = queue.dequeue() {
+        guard (row == target[0] && column == target[1]) == false else { break }
+        
+        for (nextRow, nextColumn) in zip([-2, -1, 1, 2, 2, 1, -1, -2],
+                                         [1, 2, 2, 1, -1, -2, -2, -1]) {
+            let nextRow = row + nextRow
+            let nextColumn = column + nextColumn
+            
+            guard nextRow >= 0, nextRow < length,
+                  nextColumn >= 0, nextColumn < length,
+                  board[nextRow][nextColumn] == 0 else { continue }
+            
+            board[nextRow][nextColumn] = board[row][column] + 1
+            queue.enqueue((nextRow, nextColumn))
+        }
+    }
+    
+    result += "\(board[target[0]][target[1]])\n"
 }
+
+print(result)
