f7b19c75727f07d9a0e161f756985041b4d99c0c
GUNDY <106914201+Gundy93@users.noreply.github.com>
2023-12-19 12:27:25 +0900

    Update 5014_스타트링크.swift

--- Gundy/BAEKJOON/5014_스타트링크.swift
+++ Gundy/BAEKJOON/5014_스타트링크.swift
@@ -1,53 +1,80 @@
 struct Queue<T> {
-    private var left = [T]()
-    private var right: [T]
-    var isEmpty: Bool {
-        return left.isEmpty && right.isEmpty
-    }
-
-    init(_ elements: [T] = []) {
-        right = elements
-    }
-
-    mutating func enqueue(_ element: T) {
-        right.append(element)
-    }
-
-    mutating func dequeue() -> T? {
-        if left.isEmpty {
-            if right.isEmpty {
-                return nil
-            }
-            left = right.reversed()
-            right = []
+    
+    final class Node {
+        
+        let value: T
+        var next: Node?
+        
+        init(value: T) {
+            self.value = value
         }
-        return left.removeLast()
     }
-}
-
-func solution() {
-    let input = readLine()!.split(separator: " ").compactMap({ Int($0) })
-    let heightOfBuilding = input[0]
-    let now = input[1]
-    let target = input[2]
-    let up = input[3]
-    let down = input[4]
-    var needVisit = Queue<(floor: Int, count: Int)>([(now, 0)])
-    var visited = Set<Int>()
-    while needVisit.isEmpty == false {
-        let floor = needVisit.dequeue()!
-        if floor.0 == target {
-            print(floor.1)
+    
+    private var head: Node?
+    private var tail: Node?
+    var first: T? {
+        return head?.value
+    }
+    var last: T? {
+        return tail?.value
+    }
+    var isEmpty: Bool {
+        return head == nil
+    }
+    
+    mutating func enqueue(_ newElement: T) {
+        let node = Node(value: newElement)
+        
+        guard let lastNode = tail else {
+            head = node
+            tail = node
+            
             return
         }
-        guard floor.0 > 0,
-              floor.0 <= heightOfBuilding,
-              visited.contains(floor.0) == false else { continue }
-        visited.insert(floor.0)
-        needVisit.enqueue((floor.0 + up, floor.1 + 1))
-        needVisit.enqueue((floor.0 - down, floor.1 + 1))
+        
+        lastNode.next = node
+        tail = node
     }
-    print("use the stairs")
+    
+    mutating func dequeue() -> T? {
+        let element = first
+        
+        if head?.next == nil {
+            tail = nil
+        }
+        
+        head = head?.next
+        
+        return element
+    }
 }
 
-solution()
+var result = "use the stairs"
+let input = readLine()!.split(separator: " ").map { Int($0)! }
+let height = input[0]
+let start = input[1]
+let target = input[2]
+let up = input[3]
+let down = input[4]
+var visited = Set<Int>()
+var queue = Queue<(floor: Int, count: Int)>()
+
+queue.enqueue((start, 0))
+
+while let (floor, count) = queue.dequeue() {
+    guard 1...height ~= floor,
+          visited.contains(floor) == false else { continue }
+    
+    visited.insert(floor)
+    
+    guard floor != target else {
+        result = String(count)
+        
+        break
+    }
+    
+    queue.enqueue((floor + up, count + 1))
+    queue.enqueue((floor - down, count + 1))
+}
+
+print(result)
