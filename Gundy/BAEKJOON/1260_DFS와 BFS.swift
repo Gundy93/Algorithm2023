55f91b2fe320e60556da61bfdc6867b89af428a2
GUNDY <106914201+Gundy93@users.noreply.github.com>
2023-12-22 00:53:11 +0900

    Update 1260_DFS와 BFS.swift

--- Gundy/BAEKJOON/1260_DFS와 BFS.swift
+++ Gundy/BAEKJOON/1260_DFS와 BFS.swift
@@ -1,55 +1,50 @@
-let input = readLine()!.split(separator: " ").compactMap { Int($0) }
-let countOfNodes = input[0]
-let countOfLinks = input[1]
-let startNode = input[2]
-var linking = [Int: Set<Int>]()
+let input = readLine()!.split(separator: " ").map { Int($0)! }
+var links = [Int: Set<Int>]()
 
-for _ in 1...countOfLinks {
-    let link = readLine()!.split(separator: " ").compactMap { Int($0) }
-
-    linking[link[0], default: []].insert(link[1])
-    linking[link[1], default: []].insert(link[0])
+for _ in stride(from: 1, through: input[1], by: 1) {
+    let link = readLine()!.split(separator: " ").map { Int($0)! }
+    
+    links[link[0], default: []].insert(link[1])
+    links[link[1], default: []].insert(link[0])
 }
 
-func dfs() {
-    var isVisited = Array(repeating: false, count: countOfNodes + 1)
-    var needVisit = [startNode]
-    var result = [Int]()
-
-    while needVisit.isEmpty == false {
-        let node = needVisit.removeLast()
-
-        guard isVisited[node] == false else {
-            continue
+func dfs(_ node: Int) -> String {
+    var visited = [Int]()
+    var stack = [node]
+    
+    while let current = stack.popLast() {
+        guard visited.contains(current) == false else { continue }
+        
+        visited.append(current)
+        
+        for node in links[current, default: []].sorted(by: >) {
+            stack.append(node)
         }
-
-        isVisited[node] = true
-        needVisit += linking[node, default: []].sorted(by: >)
-        result.append(node)
     }
-
-    print(result.map(String.init).joined(separator: " "))
+    
+    return visited.map(String.init).joined(separator: " ")
 }
 
-func bfs() {
-    var isVisited = Array(repeating: false, count: countOfNodes + 1)
-    var needVisit = [startNode]
-    var result = [Int]()
-
-    while needVisit.isEmpty == false {
-        let node = needVisit.removeLast()
-
-        guard isVisited[node] == false else {
-            continue
+func bfs(_ node: Int) -> String {
+    var visited = [Int]()
+    var queue = [node]
+    var index = 0
+    
+    while queue.count > index {
+        let current = queue[index]
+        
+        index += 1
+        
+        guard visited.contains(current) == false else { continue }
+        
+        visited.append(current)
+        
+        for node in links[current, default: []].sorted() {
+            queue.append(node)
         }
-
-        isVisited[node] = true
-        needVisit = linking[node, default: []].sorted(by: >) + needVisit
-        result.append(node)
     }
-
-    print(result.map(String.init).joined(separator: " "))
+    
+    return visited.map(String.init).joined(separator: " ")
 }
 
-dfs()
-bfs()
+print("\(dfs(input[2]))\n\(bfs(input[2]))")
