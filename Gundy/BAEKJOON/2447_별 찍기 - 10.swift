592d04ef357f046d57b4c5ec0e0fddf36d451475
GUNDY <106914201+Gundy93@users.noreply.github.com>
2023-12-29 14:51:26 +0900

    Update 2447_별 찍기 - 10.swift

--- Gundy/BAEKJOON/2447_별 찍기 - 10.swift
+++ Gundy/BAEKJOON/2447_별 찍기 - 10.swift
@@ -1,21 +1,15 @@
-let size = Int(readLine()!)!
-var stars = Array(repeating: Array(repeating: "*", count: size), count: size)
-
-func removeCenter(at point: (Int, Int), size: Int) {
-    let term = size / 3
+func makeStars(size: Int) -> [String] {
+    guard size > 1 else { return ["*"] }
     
-    for row in point.0 + term..<point.0 + term * 2 {
-        for column in point.1 + term..<point.1 + term * 2 {
-            stars[row][column] = " "
-        }
-    }
+    let stars = makeStars(size: size / 3)
+    let space = String(repeating: " ", count: size / 3)
+    var result = [String]()
     
-    guard term > 1 else { return }
+    stars.forEach { result.append($0 + $0 + $0) }
+    stars.forEach { result.append($0 + space + $0) }
+    stars.forEach { result.append($0 + $0 + $0) }
     
-    for (deltaRow, deltaColumn) in [(0, 0), (0, term), (0, term * 2), (term, 0), (term, term * 2), (term * 2, 0), (term * 2, term), (term * 2 , term * 2)] {
-        removeCenter(at: (point.0 + deltaRow, point.1 + deltaColumn), size: term)
-    }
+    return result
 }
 
-removeCenter(at: (0, 0), size: size)
-print(stars.map { $0.joined() }.joined(separator: "\n"))
+print(makeStars(size: Int(readLine()!)!).joined(separator: "\n"))
