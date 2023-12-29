0946e35a852394409b5f1ab7fa505755ee0a094f
GUNDY <106914201+Gundy93@users.noreply.github.com>
2023-12-29 14:45:18 +0900

    Update 2447_별 찍기 - 10.swift

--- Gundy/BAEKJOON/2447_별 찍기 - 10.swift
+++ Gundy/BAEKJOON/2447_별 찍기 - 10.swift
@@ -1,26 +1,21 @@
-let count = Int(readLine()!)!
-var stars = Array(repeating: Array(repeating: " ", count: count), count: count)
+let size = Int(readLine()!)!
+var stars = Array(repeating: Array(repeating: "*", count: size), count: size)
 
-func recursion(_ row: Int, _ column: Int, _ term: Int) {
-    guard  term > 1 else {
-        stars[row][column] = "*"
-        
-        return
+func removeCenter(at point: (Int, Int), size: Int) {
+    let term = size / 3
+    
+    for row in point.0 + term..<point.0 + term * 2 {
+        for column in point.1 + term..<point.1 + term * 2 {
+            stars[row][column] = " "
+        }
     }
     
-    for x in stride(from: 0, to: term, by: term / 3) {
-        for y in stride(from: 0, to: term, by: term / 3) {
-            guard (x == term / 3 && y == term / 3) == false else { continue }
-            
-            recursion(row + x, column + y, term / 3)
-        }
+    guard term > 1 else { return }
+    
+    for (deltaRow, deltaColumn) in [(0, 0), (0, term), (0, term * 2), (term, 0), (term, term * 2), (term * 2, 0), (term * 2, term), (term * 2 , term * 2)] {
+        removeCenter(at: (point.0 + deltaRow, point.1 + deltaColumn), size: term)
     }
 }
 
-recursion(0, 0, count)
-
-var result = String()
-
-stars.forEach { result += $0.joined() + "\n" }
-
-print(result)
+removeCenter(at: (0, 0), size: size)
+print(stars.map { $0.joined() }.joined(separator: "\n"))
