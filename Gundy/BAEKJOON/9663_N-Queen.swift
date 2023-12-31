760adb6a2cabc0872e977e1ed161b104e193437d
GUNDY <106914201+Gundy93@users.noreply.github.com>
2023-12-31 14:49:02 +0900

    Update 9663_N-Queen.swift

--- Gundy/BAEKJOON/9663_N-Queen.swift
+++ Gundy/BAEKJOON/9663_N-Queen.swift
@@ -1,33 +1,52 @@
-let count = Int(readLine()!)!
-var columns = Array(repeating: true, count: count)
-var upwards = Array(repeating: true, count: count * 2 - 1)
-var downwards = Array(repeating: true, count: count * 2 - 1)
+let length = Int(readLine()!)!
+var columns = Array(repeating: false, count: length)
+var rightUp = Array(repeating: false, count: length * 2 - 1)
+var rightDown = Array(repeating: false, count: length * 2 - 1)
 var result = 0
 
-func backTracking(_ row: Int) {
-    guard row < count else {
+func backtracking(row: Int) {
+    guard row < length else {
         result += 1
         
         return
     }
     
-    for column in 0...count - 1 {
-        guard columns[column],
-              upwards[row + column],
-              downwards[count - 1 - column + row] else { continue }
-        
-        columns[column] = false
-        upwards[row + column] = false
-        downwards[count - 1 - column + row] = false
-        
-        backTracking(row + 1)
+    for column in 0...length - 1 {
+        guard columns[column] == false,
+              rightUp[row + column] == false,
+              rightDown[length - 1 + row - column] == false else { continue }
         
         columns[column] = true
-        upwards[row + column] = true
-        downwards[count - 1 - column + row] = true
+        rightUp[row + column] = true
+        rightDown[length - 1 + row - column] = true
+        backtracking(row: row + 1)
+        rightDown[length - 1 + row - column] = false
+        rightUp[row + column] = false
+        columns[column] = false
     }
 }
 
-backTracking(0)
+for column in stride(from: 0, to: length / 2, by: 1) {
+    columns[column] = true
+    rightUp[column] = true
+    rightDown[length - 1 - column] = true
+    backtracking(row: 1)
+    rightDown[length - 1 - column] = false
+    rightUp[column] = false
+    columns[column] = false
+}
+
+result *= 2
+
+if length % 2 == 1 {
+    let column = length / 2
+    columns[column] = true
+    rightUp[column] = true
+    rightDown[length - 1 - column] = true
+    backtracking(row: 1)
+    rightDown[length - 1 - column] = false
+    rightUp[column] = false
+    columns[column] = false
+}
 
 print(result)
