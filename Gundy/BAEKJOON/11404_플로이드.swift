d3e65d6a92d693604c0523d359370c75124b8eee
GUNDY <106914201+Gundy93@users.noreply.github.com>
2024-01-01 20:09:50 +0900

    Update 11404_플로이드.swift

--- Gundy/BAEKJOON/11404_플로이드.swift
+++ Gundy/BAEKJOON/11404_플로이드.swift
@@ -1,26 +1,40 @@
+let maxCost = Int.max / 2
 let numberOfCities = Int(readLine()!)!
-let numberOfBuses = Int(readLine()!)!
-var cities = Array(repeating: Array(repeating: Int.max, count: numberOfCities), count: numberOfCities)
-for _ in 1...numberOfBuses {
-    let bus = readLine()!.split(separator: " ").map(String.init).compactMap(Int.init)
-    let (first, second, cost) = (bus[0] - 1, bus[1] - 1, bus[2])
-    if cities[first][second] == Int.max || cities[first][second] > cost {
-        cities[first][second] = cost
-    }
+let count = Int(readLine()!)!
+var buses = [(Int, Int, Int)]()
+
+for _ in 1...count {
+    let bus = readLine()!.split(separator: " ").map { Int($0)! }
+    
+    buses.append((bus[0], bus[1], bus[2]))
 }
-for center in 0...numberOfCities - 1 {
-    cities[center][center] = 0
-    for first in 0...numberOfCities - 1 {
-        let firstCost = cities[first][center]
-        guard first != center, firstCost < Int.max else { continue }
-        for second in 0...numberOfCities - 1 {
-            let secondCost = cities[center][second]
-            guard second != first, secondCost < Int.max else { continue }
-            let cost = firstCost + secondCost
-            if cities[first][second] > cost {
-                cities[first][second] = cost
-            }
+
+var costs = Array(repeating: Array(repeating: maxCost, count: numberOfCities + 1), count: numberOfCities + 1)
+
+for city in 1...numberOfCities {
+    costs[city][city] = 0
+}
+
+for (start, end, cost) in buses {
+    costs[start][end] = min(costs[start][end], cost)
+}
+
+for mid in 1...numberOfCities {
+    for start in 1...numberOfCities {
+        guard start != mid else { continue }
+        
+        for end in 1...numberOfCities {
+            guard end != start else { continue }
+            
+            costs[start][end] = min(costs[start][end], costs[start][mid] + costs[mid][end])
         }
     }
 }
-cities.forEach({ print($0.map({ $0 == Int.max ? "0" : String($0) }).joined(separator: " ")) })
+
+var result = String()
+
+for city in 1...numberOfCities {
+    result += costs[city][1...numberOfCities].map { $0 == maxCost ? "0" : String($0) }.joined(separator: " ") + "\n"
+}
+
+print(result)
