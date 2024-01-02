2bdb3054544bd436878785ba9c5d33dbc29a8131
GUNDY <106914201+Gundy93@users.noreply.github.com>
2024-01-02 16:17:59 +0900

    Update 15651_N과 M (3).swift

--- Gundy/BAEKJOON/15651_N과 M (3).swift
+++ Gundy/BAEKJOON/15651_N과 M (3).swift
@@ -1,18 +1,19 @@
 let input = readLine()!.split(separator: " ").map { Int($0)! }
-var numbers = [String]()
+let numbers = (1...input[0]).map(String.init)
+var stack = [String]()
 var result = String()
 
 func backtracking() {
-    guard numbers.count < input[1] else {
-        result += numbers.joined(separator: " ") + "\n"
+    guard stack.count < input[1] else {
+        result += stack.joined(separator: " ") + "\n"
         
         return
     }
     
-    for number in 1...input[0] {
-        numbers.append(String(number))
+    for index in 0...input[0] - 1 {
+        stack.append(numbers[index])
         backtracking()
-        numbers.removeLast()
+        stack.removeLast()
     }
 }
 
