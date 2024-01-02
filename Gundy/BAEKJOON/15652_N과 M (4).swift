0bd1f5733e0b7e42583cc0260d85018527ebd2ab
GUNDY <106914201+Gundy93@users.noreply.github.com>
2024-01-02 14:51:52 +0900

    Update 15652_N과 M (4).swift

--- Gundy/BAEKJOON/15652_N과 M (4).swift
+++ Gundy/BAEKJOON/15652_N과 M (4).swift
@@ -1,25 +1,22 @@
+let input = readLine()!.split(separator: " ").map { Int($0)! }
+var numbers = [String]()
 var result = String()
 
-func recursion(_ numbers: [Int], _ count: Int, _ maximum: Int) {
-    guard numbers.count < count else {
-        result += numbers.map(String.init).joined(separator: " ") + "\n"
+func backtracking() {
+    guard numbers.count < input[1] else {
+        result += numbers.joined(separator: " ") + "\n"
         
         return
     }
     
-    var start = 1
+    let last = Int(numbers.last ?? "1")!
     
-    if let last = numbers.last {
-        start = last
-    }
-    
-    for number in stride(from: start, through: maximum, by: 1) {
-        recursion(numbers + [number], count, maximum)
+    for number in last...input[0] {
+        numbers.append(String(number))
+        backtracking()
+        numbers.removeLast()
     }
 }
 
-let input = readLine()!.split(separator: " ").map { Int($0)! }
-
-recursion([], input[1], input[0])
-
+backtracking()
 print(result)
