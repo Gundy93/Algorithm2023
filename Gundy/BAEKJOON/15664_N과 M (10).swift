4f50696323208e04ab0cb4b299a17179ccc9bdde
GUNDY <106914201+Gundy93@users.noreply.github.com>
2024-01-02 16:44:25 +0900

    Update 15664_N과 M (10).swift

--- Gundy/BAEKJOON/15664_N과 M (10).swift
+++ Gundy/BAEKJOON/15664_N과 M (10).swift
@@ -1,19 +1,32 @@
-let targetCount = readLine()!.split(separator: " ").compactMap({ Int($0) })[1]
-let numbers = readLine()!.split(separator: " ").compactMap({ Int($0) }).sorted()
-var result = Set<[Int]>()
+let input = readLine()!.split(separator: " ").map { Int($0)! }
+let numbers = readLine()!.split(separator: " ").map(String.init).sorted { Int($0)! < Int($1)! }
+var stack = [String]()
+var indices = [Int]()
+var result = String()
+var checked = Set<String>()
 
-func backTracking(_ index: Int, _ indices: [Int]) {
-    if indices.count == targetCount {
-        let numberList = indices.map({ numbers[$0] })
-        guard result.contains(numberList) == false else { return }
-        result.insert(numberList)
-        print(numberList.map({ String($0) }).joined(separator: " "))
+func backtracking() {
+    guard stack.count < input[1] else {
+        let current = stack.joined(separator: " ")
+        
+        if checked.contains(current) == false {
+            checked.insert(current)
+            result += current + "\n"
+        }
+        
         return
     }
-    for nextIndex in index..<numbers.count {
-        guard indices.contains(nextIndex) == false else { continue }
-        backTracking(nextIndex, indices + [nextIndex])
+    
+    let last = indices.last ?? -1
+    
+    for index in stride(from: last + 1, to: input[0], by: 1) {
+        indices.append(index)
+        stack.append(numbers[index])
+        backtracking()
+        stack.removeLast()
+        indices.removeLast()
     }
 }
 
-backTracking(0, [])
+backtracking()
+print(result)
