609c27bc96386e0c6bb7da59d2b7b78210d9caf1
GUNDY <106914201+Gundy93@users.noreply.github.com>
2024-01-08 16:35:28 +0900

    Update 15683_감시.swift

--- Gundy/BAEKJOON/15683_감시.swift
+++ Gundy/BAEKJOON/15683_감시.swift
@@ -1,104 +1,135 @@
-enum Side {
+enum Direction: CaseIterable {
     case up
     case down
-    case left
     case right
+    case left
 }
 
-func watch(from point: (row: Int, column: Int), side: Side, board: inout [[Int]])  {
-    switch side {
-    case .up:
-        for row in stride(from: point.row - 1, through: 0, by: -1) {
-            guard board[row][point.column] != 6 else { return }
-            board[row][point.column] = -1
-        }
-    case .down:
-        for row in stride(from: point.row + 1, to: size[0], by: 1) {
-            guard board[row][point.column] != 6 else { return }
-            board[row][point.column] = -1
-        }
-    case .left:
-        for column in stride(from: point.column - 1, through: 0, by: -1) {
-            guard board[point.row][column] != 6 else { return }
-            board[point.row][column] = -1
-        }
-    case .right:
-        for column in stride(from: point.column + 1, to: size[1], by: 1) {
-            guard board[point.row][column] != 6 else { return }
-            board[point.row][column] = -1
+let input = readLine()!.split(separator: " ").map { Int($0)! }
+var room = [[Int]]()
+
+for _ in 1...input[0] {
+    room.append(readLine()!.split(separator: " ").map { Int($0)! })
+}
+
+var count = input[0] * input[1]
+var canWatch = Array(repeating: Array(repeating: false, count: input[1]), count: input[0])
+var cctvs = [(Int, Int)]()
+
+for row in 0..<input[0] {
+    for column in 0..<input[1] {
+        if 1...5 ~= room[row][column] {
+            cctvs.append((row, column))
+            canWatch[row][column] = true
+            count -= 1
+        } else if room[row][column] == 6 {
+            count -= 1
         }
     }
 }
 
-func recursion(current index: Int, board: [[Int]]) {
-    guard index < needVisit.count else {
-        var count = 0
-        for row in 0..<size[0] {
-            for column in 0..<size[1] {
-                if board[row][column] == 0 {
-                    count += 1
-                }
-            }
+var result = count
+var index = 0
+
+func backtracking() {
+    result = min(result, count)
+    
+    guard index < cctvs.count else { return }
+    
+    let (row, column) = cctvs[index]
+    var points = [(Int, Int)]()
+    
+    index += 1
+    
+    func reset() {
+        count += points.count
+        points.forEach { (row, column) in
+            canWatch[row][column] = false
         }
-        result.append(count)
-        return
+        points.removeAll()
     }
-    let point = needVisit[index]
-    switch point.value {
+    
+    switch room[row][column] {
     case 1:
-        for side in [Side.up, .down, .left, .right] {
-            var board = board
-            watch(from: (point.row, point.column), side: side, board: &board)
-            recursion(current: index + 1, board: board)
+        for direction in Direction.allCases {
+            points += count(direction: direction, row: row, column: column)
+            backtracking()
+            reset()
         }
     case 2:
-        for side in [(Side.up, Side.down), (Side.left, Side.right)] {
-            var board = board
-            watch(from: (point.row, point.column), side: side.0, board: &board)
-            watch(from: (point.row, point.column), side: side.1, board: &board)
-            recursion(current: index + 1, board: board)
+        for directions in [[Direction.up, Direction.down],
+                           [Direction.left, Direction.right]] {
+            directions.forEach { direction in
+                points += count(direction: direction, row: row, column: column)
+            }
+            backtracking()
+            reset()
         }
     case 3:
-        for side in [(Side.up, Side.right), (Side.right, Side.down), (Side.down, Side.left), (Side.left, Side.up)] {
-            var board = board
-            watch(from: (point.row, point.column), side: side.0, board: &board)
-            watch(from: (point.row, point.column), side: side.1, board: &board)
-            recursion(current: index + 1, board: board)
+        for directions in [[Direction.up, Direction.right],
+                           [Direction.right, Direction.down],
+                           [Direction.down, Direction.left],
+                           [Direction.left, Direction.up]] {
+            directions.forEach { direction in
+                points += count(direction: direction, row: row, column: column)
+            }
+            backtracking()
+            reset()
         }
     case 4:
-        for sides in [[Side.up, .right, .down], [Side.right, .down, .left], [Side.down, .left, .up], [Side.left, .up, .right]] {
-            var board = board
-            for side in sides {
-                watch(from: (point.row, point.column), side: side, board: &board)
+        for directions in [[Direction.up, Direction.right, Direction.down],
+                           [Direction.right, Direction.down, Direction.left],
+                           [Direction.down, Direction.left, Direction.up],
+                           [Direction.left, Direction.up, Direction.right]] {
+            directions.forEach { direction in
+                points += count(direction: direction, row: row, column: column)
             }
-            recursion(current: index + 1, board: board)
+            backtracking()
+            reset()
         }
     default:
-        var board = board
-        for side in [Side.up, .down, .left, .right] {
-            watch(from: (point.row, point.column), side: side, board: &board)
+        Direction.allCases.forEach { direction in
+            points += count(direction: direction, row: row, column: column)
         }
-        recursion(current: index + 1, board: board)
+        backtracking()
+        reset()
     }
+    
+    index -= 1
 }
 
-let size = readLine()!.split(separator: " ").compactMap({ Int($0) })
-var board = [[Int]]()
-for _ in 1...size[0] {
-    board.append(readLine()!.split(separator: " ").compactMap({ Int($0) }))
-}
-
-var needVisit = [(row: Int, column: Int, value: Int)]()
-for row in 0..<size[0] {
-    for column in 0..<size[1] {
-        if 1...5 ~= board[row][column] {
-            needVisit.append((row, column, board[row][column]))
+func count(direction: Direction, row: Int, column: Int) -> [(Int, Int)] {
+    var delta = (0, 0)
+    
+    switch direction {
+    case .up:
+        delta = (-1, 0)
+    case .down:
+        delta = (1, 0)
+    case .right:
+        delta = (0, 1)
+    case .left:
+        delta = (0, -1)
+    }
+    
+    var row = row + delta.0
+    var column = column + delta.1
+    var points = [(Int, Int)]()
+    
+    while 0..<input[0] ~= row,
+          0..<input[1] ~= column,
+          room[row][column] != 6 {
+        if canWatch[row][column] == false {
+            canWatch[row][column] = true
+            count -= 1
+            points.append((row, column))
         }
+        row += delta.0
+        column += delta.1
     }
+    
+    return points
 }
 
-var result = [Int]()
-
-recursion(current: 0, board: board)
-
-print(result.sorted().first!)
+backtracking()
+print(result)
