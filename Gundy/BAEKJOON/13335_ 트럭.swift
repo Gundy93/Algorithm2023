e795a762382feb493e8bffd632edb94f8dfbf0c3
GUNDY <106914201+Gundy93@users.noreply.github.com>
2024-01-09 14:07:01 +0900

    Update 13335_ 트럭.swift

--- Gundy/BAEKJOON/13335_ 트럭.swift
+++ Gundy/BAEKJOON/13335_ 트럭.swift
@@ -1,28 +1,77 @@
+struct Queue<T> {
+    
+    final class Node {
+        
+        let value: T
+        var next: Node?
+        
+        init(value: T) {
+            self.value = value
+        }
+    }
+    
+    private var head: Node?
+    private var tail: Node?
+    var first: T? {
+        return head?.value
+    }
+    var last: T? {
+        return tail?.value
+    }
+    var isEmpty: Bool {
+        return head == nil
+    }
+    
+    mutating func enqueue(_ newElement: T) {
+        let node = Node(value: newElement)
+        
+        guard let lastNode = tail else {
+            head = node
+            tail = node
+            
+            return
+        }
+        
+        lastNode.next = node
+        tail = node
+    }
+    
+    mutating func dequeue() -> T? {
+        let element = first
+        
+        if head?.next == nil {
+            tail = nil
+        }
+        
+        head = head?.next
+        
+        return element
+    }
+}
+
 let input = readLine()!.split(separator: " ").map { Int($0)! }
 let (numberOfTrucks, length, limit) = (input[0], input[1], input[2])
 let trucks = readLine()!.split(separator: " ").map { Int($0)! }
-var index = 0
 var weight = 0
 var result = 0
-var times = [Int]()
+var queue = Queue<(Int, Int)>()
 
 for truck in trucks {
     while weight + truck > limit {
-        let first = trucks[index]
+        let (time, first) = queue.dequeue()!
         
-        result = times[index]
+        result = time
         weight -= first
-        index += 1
     }
     
-    times.append(result + length)
+    queue.enqueue((result + length, truck))
     result += 1
     weight += truck
     
-    while index < times.count,
-          result >= times[index] {
-        weight -= trucks[index]
-        index += 1
+    while let (time, first) = queue.first,
+          result >= time {
+        weight -= first
+        _ = queue.dequeue()
     } 
 }
 
