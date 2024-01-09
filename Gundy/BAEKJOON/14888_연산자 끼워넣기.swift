f9c05604cfaec11f38a6ec122ae5563eaebcf3eb
GUNDY <106914201+Gundy93@users.noreply.github.com>
2024-01-09 15:42:28 +0900

    Update 14888_연산자 끼워넣기.swift

--- Gundy/BAEKJOON/14888_연산자 끼워넣기.swift
+++ Gundy/BAEKJOON/14888_연산자 끼워넣기.swift
@@ -3,31 +3,11 @@ var minimum = Int.max
 let countOfNumbers = Int(readLine()!)!
 let numbers = readLine()!.split(separator: " ").map { Int($0)! }
 var signs = readLine()!.split(separator: " ").map { Int($0)! }
-var index = countOfNumbers - 1
-var stack = [(Int, Int)]()
+var index = 1
+var result = numbers[0]
 
 func backtracking() {
-    guard index > 0 else {
-        var result = numbers[0]
-        var index = stack.count - 1
-        
-        while index >= 0 {
-            let (number, sign) = stack[index]
-            
-            switch sign {
-            case 0:
-                result += number
-            case 1:
-                result -= number
-            case 2:
-                result *= number
-            default:
-                result /= number
-            }
-            
-            index -= 1
-        }
-        
+    guard index < countOfNumbers else {
         maximum = max(maximum, result)
         minimum = min(minimum, result)
         
@@ -36,19 +16,32 @@ func backtracking() {
     
     let number = numbers[index]
     
-    index -= 1
+    index += 1
     
     for sign in 0..<4 {
         guard signs[sign] >= 1 else { continue }
         
+        let origin = result
+        
         signs[sign] -= 1
-        stack.append((number, sign))
+        
+        switch sign {
+        case 0:
+            result += number
+        case 1:
+            result -= number
+        case 2:
+            result *= number
+        default:
+            result /= number
+        }
+        
         backtracking()
-        stack.removeLast()
         signs[sign] += 1
+        result = origin
     }
     
-    index += 1
+    index -= 1
 }
 
 backtracking()
