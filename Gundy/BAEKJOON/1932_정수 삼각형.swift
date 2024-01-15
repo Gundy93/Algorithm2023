08c0e8664cf29acf9afb849febc6d50060a50580
GUNDY <106914201+Gundy93@users.noreply.github.com>
2024-01-15 22:09:59 +0900

    Update 1932_정수 삼각형.swift

--- Gundy/BAEKJOON/1932_정수 삼각형.swift
+++ Gundy/BAEKJOON/1932_정수 삼각형.swift
@@ -1,22 +1,17 @@
-let count = Int(readLine()!)!
-var floors = [[Int]]()
+let height = Int(readLine()!)!
+var pyramid = [[Int]]()
 
-for _ in 1...count {
-    floors.append(readLine()!.split(separator: " ").map { Int($0)! })
-}
-
-var dp = Array(1...count).map { Array(repeating: 0, count: $0) }
-
-dp[0] = floors[0]
-
-for row in stride(from: 1, to: count, by: 1) {
-    dp[row][0] = dp[row - 1][0] + floors[row][0]
-    
-    for column in stride(from: 1, to: dp[row].count - 1, by: 1) {
-        dp[row][column] = max(dp[row - 1][column - 1], dp[row - 1][column]) + floors[row][column]
+(0..<height)
+    .forEach { _ in
+        pyramid.append(readLine()!.split(separator: " ").map { Int($0)! })
     }
-    
-    dp[row][dp[row].count - 1] = dp[row - 1][dp[row].count - 2] + floors[row][dp[row].count - 1]
+
+var dp = Array(repeating: Array(repeating: 0, count: height + 1), count: height + 1)
+
+for row in 0..<height {
+    for column in 1...pyramid[row].count {
+        dp[row + 1][column] = max(dp[row][column - 1], dp[row][column]) + pyramid[row][column - 1]
+    }
 }
 
-print(dp.last!.max()!)
+print(dp[height].max()!)
