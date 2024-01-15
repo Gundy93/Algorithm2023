610747eb56632e541d06fc21671c972a9bcc9949
GUNDY <106914201+Gundy93@users.noreply.github.com>
2024-01-15 16:15:26 +0900

    Update 11652_카드.swift

--- Gundy/BAEKJOON/11652_카드.swift
+++ Gundy/BAEKJOON/11652_카드.swift
@@ -1,8 +1,9 @@
 let count = Int(readLine()!)!
-var cards = [Int: Int]()
+var cards = [Int : Int]()
 
-for _ in 1...count {
-    cards[Int(readLine()!)!, default: 0] += 1
-}
+(0..<count)
+    .forEach { _ in
+        cards[Int(readLine()!)!, default: 0] += 1
+    }
 
-print(cards.max { $0.value != $1.value ? $0.value < $1.value : $0.key > $1.key }!.key)
+print(cards.sorted { $0.value == $1.value ? $0.key < $1.key : $0.value > $1.value }[0].key)
