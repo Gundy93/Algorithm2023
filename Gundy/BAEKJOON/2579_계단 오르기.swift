435392e9cc9a5d46fa9ecc789c9bf3de6e351ee2
GUNDY <106914201+Gundy93@users.noreply.github.com>
2024-01-16 22:17:26 +0900

    Update 2579_계단 오르기.swift

--- Gundy/BAEKJOON/2579_계단 오르기.swift
+++ Gundy/BAEKJOON/2579_계단 오르기.swift
@@ -5,20 +5,18 @@ for _ in 1...count {
     steps.append(Int(readLine()!)!)
 }
 
-var dp = Array(repeating: 0, count: count)
-
-dp[0] = steps[0]
+var dp = steps
 
 if count > 1 {
-    dp[1] = dp[0] + steps[1]
+    dp[1] += dp[0]
 }
 
 if count > 2 {
-    dp[2] = max(dp[0] + steps[2], steps[1] + steps[2])
+    dp[2] += max(dp[0], steps[1])
 }
 
 for index in stride(from: 3, to: count, by: 1) {
-    dp[index] = max(dp[index - 2] + steps[index], dp[index - 3] + steps[index - 1] + steps[index])
+    dp[index] += max(dp[index - 2], dp[index - 3] + steps[index - 1])
 }
 
 print(dp.last!)
