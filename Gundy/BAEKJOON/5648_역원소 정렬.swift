d2177200c5cc01db6214b2bb6ba069e55e54578f
GUNDY <106914201+Gundy93@users.noreply.github.com>
2024-01-16 13:55:46 +0900

    Update 5648_역원소 정렬.swift

--- Gundy/BAEKJOON/5648_역원소 정렬.swift
+++ Gundy/BAEKJOON/5648_역원소 정렬.swift
@@ -1,18 +1,9 @@
-var numbers = readLine()!.split(separator: " ").map(String.init)
-let count = Int(numbers.removeFirst())!
+let input = readLine()!.split(separator: " ").map(String.init)
+let count = Int(input[0])!
+var numbers = stride(from: 1, to: input.count, by: 1).map { Int(String(input[$0].reversed()))! }
 
 while numbers.count < count {
-    numbers += readLine()!.split(separator: " ").map(String.init)
+    numbers += readLine()!.split(separator: " ").map { Int(String($0.reversed()))! }
 }
 
-let result: [Int] = numbers.map { number in
-    var number = number
-        
-        while number.last == "0" {
-            number.removeLast()
-        }
-        
-        return Int(String(number.reversed()))!
-}
-
-print(result.sorted().map(String.init).joined(separator: "\n"))
+print(numbers.sorted().map(String.init).joined(separator: "\n"))
