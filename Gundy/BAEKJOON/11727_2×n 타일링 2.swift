4b0e5e342039dc4118380664a4a94962b397f06f
GUNDY <106914201+Gundy93@users.noreply.github.com>
2024-01-18 21:26:11 +0900

    Update 11727_2×n 타일링 2.swift

--- Gundy/BAEKJOON/11727_2×n 타일링 2.swift
+++ Gundy/BAEKJOON/11727_2×n 타일링 2.swift
@@ -1,8 +1,6 @@
 let n = Int(readLine()!)!
 var dp = Array(repeating: 1, count: n + 1)
 
-dp[1] = 1
-
 for index in stride(from: 2, through: n, by: 1) {
     dp[index] = (dp[index - 1] + dp[index - 2] * 2) % 10007
 }
