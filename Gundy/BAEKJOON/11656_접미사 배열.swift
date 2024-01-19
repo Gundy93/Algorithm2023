30738bbdbf10d62dc09f04a6314b121f7cbc6407
GUNDY <106914201+Gundy93@users.noreply.github.com>
2024-01-19 14:14:00 +0900

    Update 11656_접미사 배열.swift

--- Gundy/BAEKJOON/11656_접미사 배열.swift
+++ Gundy/BAEKJOON/11656_접미사 배열.swift
@@ -1,10 +1,11 @@
-let input = readLine()!
-var sufficis = [String.SubSequence]()
+var input = readLine()!
+var last = String(input.removeLast())
+var sufficis = [last]
 
 for count in 0..<input.count {
-    sufficis.append(input.dropFirst(count))
+    last = String(input.removeLast()) + last
+    sufficis.append(last)
 }
 
 sufficis.sort()
-
 print(sufficis.joined(separator: "\n"))
