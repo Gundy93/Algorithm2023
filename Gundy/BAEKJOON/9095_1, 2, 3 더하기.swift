cdbc190ebdadad53de8696c568caf57f1f5bcc77
GUNDY <106914201+Gundy93@users.noreply.github.com>
2024-01-29 20:53:59 +0900

    Update 9095_1, 2, 3 더하기.swift

--- Gundy/BAEKJOON/9095_1, 2, 3 더하기.swift
+++ Gundy/BAEKJOON/9095_1, 2, 3 더하기.swift
@@ -1,49 +1,20 @@
-//MARK: 재귀 풀이
+var dp = [0]
 
-func solution() {
-    var result = Set<[Int]>()
-    let number = Int(readLine()!)!
+dp.append(1)
+dp.append(2)
+dp.append(4)
 
-    func recursion(_ numbers: [Int], _ partialResult: Int) {
-        guard partialResult < number else {
-            result.insert(numbers)
-            return
-        }
-        recursion(numbers + [1], partialResult + 1)
-        if partialResult + 2 <= number {
-            recursion(numbers + [2], partialResult + 2)
-        }
-        if partialResult + 3 <= number {
-            recursion(numbers + [3], partialResult + 3)
-        }
-    }
+let count = Int(readLine()!)!
+var result = ""
 
-    recursion([], 0)
-    print(result.count)
-}
-
-let numberOfCase = Int(readLine()!)!
-for _ in 1...numberOfCase {
-    solution()
-}
-
-//MARK: DP 풀이
-
-var counter = [0, 1, 2, 4]
-let numberOfCase = Int(readLine()!)!
-
-func solution() {
+for _ in 0..<count {
     let target = Int(readLine()!)!
-    if counter.count > target {
-        print(counter[target])
-    } else {
-        for number in counter.count...target {
-            counter.append(counter[number - 1] + counter[number - 2] + counter[number - 3])
-        }
-        print(counter[target])
+    
+    while dp.count <= target {
+        dp.append(dp[dp.count - 1] + dp[dp.count - 2] + dp[dp.count - 3])
     }
+    
+    result += String(dp[target]) + "\n"
 }
 
-for _ in 1...numberOfCase {
-    solution()
-}
+print(result)
