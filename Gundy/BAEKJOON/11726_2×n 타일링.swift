58228796b67d75f608e21b01a66ec11d82ada66c
GUNDY <106914201+Gundy93@users.noreply.github.com>
2024-01-30 14:22:20 +0900

    Update 11726_2×n 타일링.swift

--- Gundy/BAEKJOON/11726_2×n 타일링.swift
+++ Gundy/BAEKJOON/11726_2×n 타일링.swift
@@ -1,10 +1,8 @@
 let n = Int(readLine()!)!
-var dp = Array(repeating: 1, count: n + 1)
+var dp = [1,2]
 
-if n > 1 {
-    for index in 2...n {
-        dp[index] = dp[index - 1] + dp[index - 2] % 10007
-    }
+while dp.count < n {
+    dp.append((dp[dp.count-1] + dp[dp.count-2]) % 10007)
 }
 
-print(dp[n])
+print(dp[n-1])
