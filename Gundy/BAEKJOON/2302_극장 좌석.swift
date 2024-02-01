f2480ba9bf2fcea6171af8ca58513e6607ec0196
GUNDY <106914201+Gundy93@users.noreply.github.com>
2024-02-01 22:54:40 +0900

    Create 2302_극장 좌석.swift

---
+++ Gundy/BAEKJOON/2302_극장 좌석.swift
@@ -0,0 +1,32 @@
+let n = Int(readLine()!)!
+let count = Int(readLine()!)!
+var fixed = [0]
+var dp = [0,1,2,3,5,8]
+var index = 6
+
+for _ in 0..<count {
+    fixed.append(Int(readLine()!)!)
+}
+
+fixed.append(n+1)
+
+func countWays(for number: Int) -> Int {
+    while index <= number {
+        dp.append(dp[index - 2] + dp[index - 1])
+        index += 1
+    }
+    
+    return dp[number]
+}
+
+var result = 1
+
+for index in 0...count {
+    let number = countWays(for: fixed[index + 1] - fixed[index] - 1)
+    
+    guard number > 1 else { continue }
+    
+    result *= number
+}
+
+print(result)
