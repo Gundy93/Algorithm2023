35d502087cb14916052be411f9930a9507f74e31
GUNDY <106914201+Gundy93@users.noreply.github.com>
2024-02-01 21:22:11 +0900

    Update 2156_포도주 시식.swift

--- Gundy/BAEKJOON/2156_포도주 시식.swift
+++ Gundy/BAEKJOON/2156_포도주 시식.swift
@@ -1,28 +1,14 @@
 let count = Int(readLine()!)!
-var wines = [Int]()
+var wines = [0,0,0]
 
-for _ in 1...count {
+for _ in 0..<count {
     wines.append(Int(readLine()!)!)
 }
 
-var dp = Array(repeating: 0, count: count)
+var dp = Array(repeating: 0, count: count + 3)
 
-dp[0] = wines[0]
-
-if count > 1 {
-    dp[1] = wines[0] + wines[1]
+for index in 3..<count + 3 {
+    dp[index] = max(dp[index - 2], dp[index - 3] + wines[index - 1]) + wines[index]
 }
 
-if count > 2 {
-    dp[2] = max(wines[0] + wines[2], wines[1] + wines[2])
-}
-
-if count > 3 {
-    dp[3] = max(dp[1] + wines[3], wines[0] + wines[2] + wines[3])
-}
-
-for index in stride(from: 4, to: count, by: 1) {
-    dp[index] = max(dp[index - 2], max(dp[index - 4], dp[index - 3]) + wines[index - 1]) + wines[index]
-}
-
-print(count > 1 ? max(dp[count - 1], dp[count - 2]) : dp[0])
+print(dp.max()!)
