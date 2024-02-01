bf62c90eac217868b2e00184ff44feb714e60e56
GUNDY <106914201+Gundy93@users.noreply.github.com>
2024-02-01 23:49:24 +0900

    Update 2302_극장 좌석.swift

--- Gundy/BAEKJOON/2302_극장 좌석.swift
+++ Gundy/BAEKJOON/2302_극장 좌석.swift
@@ -1,7 +1,7 @@
 let n = Int(readLine()!)!
 let count = Int(readLine()!)!
 var fixed = [0]
-var dp = [0,1,2,3,5,8]
+var dp = [1,1,2,3,5,8]
 var index = 6
 
 for _ in 0..<count {
@@ -22,11 +22,7 @@ func countWays(for number: Int) -> Int {
 var result = 1
 
 for index in 0...count {
-    let number = countWays(for: fixed[index + 1] - fixed[index] - 1)
-    
-    guard number > 1 else { continue }
-    
-    result *= number
+    result *= countWays(for: fixed[index + 1] - fixed[index] - 1)
 }
 
 print(result)
