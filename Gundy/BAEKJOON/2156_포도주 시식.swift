fec9519a5617873c0e4b6b9f0911c9a690d33353
GUNDY <106914201+Gundy93@users.noreply.github.com>
2024-02-01 21:56:40 +0900

    Update 2156_포도주 시식.swift

--- Gundy/BAEKJOON/2156_포도주 시식.swift
+++ Gundy/BAEKJOON/2156_포도주 시식.swift
@@ -1,14 +1,14 @@
 let count = Int(readLine()!)!
-var wines = [0,0,0]
+var wines = Array(repeating: 0, count: 4)
 
 for _ in 0..<count {
     wines.append(Int(readLine()!)!)
 }
 
-var dp = Array(repeating: 0, count: count + 3)
+var dp = Array(repeating: 0, count: count + 4)
 
-for index in 3..<count + 3 {
-    dp[index] = max(dp[index - 2], dp[index - 3] + wines[index - 1]) + wines[index]
+for index in 4..<count + 4 {
+    dp[index] = max(dp[index - 2], max(dp[index - 3], dp[index - 4]) + wines[index - 1]) + wines[index]
 }
 
 print(dp.max()!)
