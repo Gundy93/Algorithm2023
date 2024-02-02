00654fa180f179a201932c5c37eeef407794c0fc
GUNDY <106914201+Gundy93@users.noreply.github.com>
2024-02-02 14:14:37 +0900

    Update 11659_구간 합 구하기 4.swift

--- Gundy/BAEKJOON/11659_구간 합 구하기 4.swift
+++ Gundy/BAEKJOON/11659_구간 합 구하기 4.swift
@@ -1,16 +1,17 @@
 let input = readLine()!.split(separator: " ").map { Int($0)! }
 let numbers = readLine()!.split(separator: " ").map { Int($0)! }
-var dp = Array(repeating: 0, count: input[0] + 1)
+var partialsum = [0]
 
-for index in stride(from: 0, to: input[0], by: 1) {
-    dp[index + 1] = dp[index] + numbers[index]
+for index in 0..<input[0] {
+    partialsum.append(partialsum[index] + numbers[index])
 }
 
-var result = String()
+var result = ""
 
-for _ in 1...input[1] {
-    let indices = readLine()!.split(separator: " ").map { Int($0)! }
-    result += "\(dp[indices[1]] - dp[indices[0] - 1])\n"
+for _ in 0..<input[1] {
+    let range = readLine()!.split(separator: " ").map { Int($0)! }
+    
+    result += String(partialsum[range[1]] - partialsum[range[0] - 1]) + "\n"
 }
 
 print(result)
