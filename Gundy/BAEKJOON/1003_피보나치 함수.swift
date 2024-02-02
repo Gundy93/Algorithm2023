e970fc7d43b57e769e66f6a4c1a7979cfcb9dc0d
GUNDY <106914201+Gundy93@users.noreply.github.com>
2024-02-02 14:53:39 +0900

    Update 1003_피보나치 함수.swift

--- Gundy/BAEKJOON/1003_피보나치 함수.swift
+++ Gundy/BAEKJOON/1003_피보나치 함수.swift
@@ -1,24 +1,15 @@
 let count = Int(readLine()!)!
-var dp = [(1, 0), (0, 1)]
-var result = String()
+var dp = [(1,0), (0,1)]
+var result = ""
 
-(0..<count)
-    .forEach { _ in
-        let index = Int(readLine()!)!
-        
-        guard dp.count <= index else {
-            result += "\(dp[index].0) \(dp[index].1)\n"
-            
-            return
-        }
-        
-        while dp.count <= index {
-            let first = dp[dp.count - 2]
-            let second = dp[dp.count - 1]
-            dp.append((first.0 + second.0, first.1 + second.1))
-        }
-        
-        result += "\(dp[index].0) \(dp[index].1)\n"
+for _ in 0..<count {
+    let target = Int(readLine()!)!
+    
+    while dp.count <= target {
+        dp.append((dp[dp.count - 2].0 + dp[dp.count - 1].0, dp[dp.count - 2].1 + dp[dp.count - 1].1))
     }
+    
+    result += String(dp[target].0) + " " + String(dp[target].1) + "\n"
+}
 
 print(result)
