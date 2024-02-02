8d27b07a53202b4d96863cd38431b6bb85a19d11
GUNDY <106914201+Gundy93@users.noreply.github.com>
2024-02-02 13:33:26 +0900

    Update 1463_1로 만들기.swift

--- Gundy/BAEKJOON/1463_1로 만들기.swift
+++ Gundy/BAEKJOON/1463_1로 만들기.swift
@@ -1,18 +1,57 @@
-let target = Int(readLine()!)!
-var dp = Array(repeating: Int.max, count: target + 1)
-
-dp[1] = 0
-
-for number in 2..<target + 1 {
-    if number % 6 == 0 {
-        dp[number] = min(dp[number - 1], min(dp[number / 2], dp[number / 3])) + 1
-    } else if number % 3 == 0 {
-        dp[number] = min(dp[number - 1], dp[number / 3]) + 1
-    } else if number % 2 == 0 {
-        dp[number] = min(dp[number - 1], dp[number / 2]) + 1
-    } else {
-        dp[number] = dp[number - 1] + 1
+struct Queue<T> {
+    private var input = [T]()
+    private var output = [T]()
+    
+    var first: T? {
+        return output.last ?? input.first
+    }
+    var last: T? {
+        return input.last ?? output.first
+    }
+    var isEmpty: Bool {
+        return input.isEmpty && output.isEmpty
+    }
+    
+    mutating func enqueue(_ newElement: T) {
+        input.append(newElement)
+    }
+    
+    mutating func dequeue() -> T? {
+        if output.isEmpty {
+            output = input.reversed()
+            input.removeAll()
+        }
+        
+        return output.popLast()
     }
 }
 
-print(dp[target])
+let target = Int(readLine()!)!
+var visited = Set<Int>()
+var queue = Queue<(Int, Int)>()
+
+queue.enqueue((target, 0))
+
+while let (number, count) = queue.dequeue() {
+    guard number > 1 else {
+        print(count)
+        break
+    }
+
+    if number % 3 == 0,
+       visited.contains(number/3) == false {
+        visited.insert(number/3)
+        queue.enqueue((number/3, count+1))
+    }
+
+    if number % 2 == 0,
+       visited.contains(number/2) == false {
+        visited.insert(number/2)
+        queue.enqueue((number/2, count+1))
+    }
+
+    if visited.contains(number-1) == false {
+        visited.insert(number-1)
+        queue.enqueue((number-1, count+1))
+    }
+}
