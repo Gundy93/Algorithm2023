edb6eae574abd67fb679f41fd3a205087b841361
GUNDY <106914201+Gundy93@users.noreply.github.com>
2024-02-03 14:36:51 +0900

    Update 11047_동전 0.swift

--- Gundy/BAEKJOON/11047_동전 0.swift
+++ Gundy/BAEKJOON/11047_동전 0.swift
@@ -1,20 +1,14 @@
-let input = readLine()!.split(separator: " ").map { Int($0)! }
+let input = readLine()!.split(separator: " ").map{ Int($0)! }
+var target = input[1]
 var coins = [Int]()
 
-for _ in 1...input[0] {
+for _ in 0..<input[0] {
     coins.append(Int(readLine()!)!)
 }
 
-coins.sort(by: >)
-
-var target = input[1]
 var result = 0
 
-for index in 0...coins.count - 1 {
-    guard target > 0 else { break }
-    
-    let coin = coins[index]
-    
+for coin in coins.reversed() {
     result += target / coin
     target %= coin
 }
