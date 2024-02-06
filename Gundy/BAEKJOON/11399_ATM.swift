72fe27e973c6e679c06b608c5abc0651da525bd5
GUNDY <106914201+Gundy93@users.noreply.github.com>
2024-02-06 13:23:14 +0900

    Update 11399_ATM.swift

--- Gundy/BAEKJOON/11399_ATM.swift
+++ Gundy/BAEKJOON/11399_ATM.swift
@@ -1,8 +1,8 @@
 let count = Int(readLine()!)!
 var times = readLine()!.split(separator: " ").map { Int($0)! }.sorted()
 
-for index in stride(from: 1, to: count, by: 1) {
-    times[index] += times[index - 1]
+for index in 1..<count {
+    times[index] += times[index-1]
 }
 
 print(times.reduce(0, +))
