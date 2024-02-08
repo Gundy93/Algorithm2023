df03ba49d12ee44e2900918a999e68a624ceb596
GUNDY <106914201+Gundy93@users.noreply.github.com>
2024-02-08 21:14:03 +0900

    Update 1904_01타일.swift

--- Gundy/BAEKJOON/1904_01타일.swift
+++ Gundy/BAEKJOON/1904_01타일.swift
@@ -1,12 +1,8 @@
-let count = Int(readLine()!)!
-var dp = Array(repeating: 1, count: count + 1)
+let n = Int(readLine()!)!
+var dp = [1, 1]
 
-if count > 1 {
-    dp[2] = 2
-    
-    for number in stride(from: 3, through: count, by: 1) {
-        dp[number] = (dp[number - 2] + dp[number - 1]) % 15746
-    }
+while dp.count <= n {
+    dp.append((dp[dp.count-2] + dp[dp.count-1]) % 15746)
 }
 
-print(dp[count])
+print(dp[n])
