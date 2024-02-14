37e5e24b72c978b74a0da27f54481e852c026709
GUNDY <106914201+Gundy93@users.noreply.github.com>
2024-02-14 21:11:02 +0900

    Update 11399_ATM.swift

--- Gundy/BAEKJOON/11399_ATM.swift
+++ Gundy/BAEKJOON/11399_ATM.swift
@@ -1,6 +1,6 @@
 let count = Int(readLine()!)!
-var times = readLine()!.split(separator: " ").map { Int($0)! }.sorted(by: >)
-var result = 0
+let costs = readLine()!.split(separator: " ").map { Int($0)! }
 
-zip(1...count, times).forEach { result += $0 * $1 }
-print(result)
+print(costs.sorted(by: >).enumerated().reduce(0) { partialResult, cost in
+    partialResult + cost.element * (cost.offset+1)
+})
