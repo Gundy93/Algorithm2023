72f8d3406f7f8091e305650ef6ac1c8e18bc0092
GUNDY <106914201+Gundy93@users.noreply.github.com>
2024-02-16 14:20:37 +0900

    Update 11050_이항 계수 1.swift

--- Gundy/BAEKJOON/11050_이항 계수 1.swift
+++ Gundy/BAEKJOON/11050_이항 계수 1.swift
@@ -1,9 +1,12 @@
 let numbers = readLine()!.split(separator: " ").map { Int($0)! }
-let numerator = numbers[0] > 1 ? (2...numbers[0]).reduce(1, *) : 1
-var denominator = numbers[1] > 1 ? (2...numbers[1]).reduce(1, *) : 1
+var dp = Array(repeating: Array(repeating: 1, count: numbers[0] + 1), count: numbers[0] + 1)
 
-if numbers[0] - numbers[1] > 1 {
-    denominator *= (2...numbers[0] - numbers[1]).reduce(1, *)
+if numbers[0] > 1 {
+    for n in 2...numbers[0] {
+        for k in 1..<n {
+            dp[n][k] = (dp[n-1][k] + dp[n-1][k-1])
+        }
+    }
 }
 
-print(numerator / denominator)
+print(dp[numbers[0]][numbers[1]])
