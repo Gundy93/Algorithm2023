0f07682ac9fef1e43c90f4e4253f346fee4ecd67
GUNDY <106914201+Gundy93@users.noreply.github.com>
2024-02-26 11:07:56 +0900

    Update 2230_수 고르기.swift

--- Gundy/BAEKJOON/2230_수 고르기.swift
+++ Gundy/BAEKJOON/2230_수 고르기.swift
@@ -1,76 +1,25 @@
-import Foundation
-
-final class FileIO {
-    private var buffer:[UInt8]
-    private var index: Int
-    
-    init(fileHandle: FileHandle = FileHandle.standardInput) {
-        buffer = Array(fileHandle.readDataToEndOfFile())+[UInt8(0)]
-        index = 0
-    }
-    
-    @inline(__always) private func read() -> UInt8 {
-        defer { index += 1 }
-        
-        return buffer.withUnsafeBufferPointer { $0[index] }
-    }
-    
-    @inline(__always) func readInt() -> Int {
-        var sum = 0
-        var now = read()
-        var isPositive = true
-        
-        while now == 10
-                || now == 32 { now = read() }
-        if now == 45{ isPositive.toggle(); now = read() }
-        while now >= 48, now <= 57 {
-            sum = sum * 10 + Int(now-48)
-            now = read()
-        }
-        
-        return sum * (isPositive ? 1:-1)
-    }
-    
-    @inline(__always) func readString() -> String {
-        var str = ""
-        var now = read()
-        
-        while now == 10
-                || now == 32 { now = read() }
-        
-        while now != 10
-                && now != 32 && now != 0 {
-            str += String(bytes: [now], encoding: .ascii)!
-            now = read()
-        }
-        
-        return str
-    }
-}
-
-let fileIO = FileIO()
-let count = fileIO.readInt()
-let minimum = fileIO.readInt()
+let input = readLine()!.split(separator: " ").map { Int($0)! }
 var numbers = [Int]()
-var result = Int.max
 
-for _ in 1...count {
-    numbers.append(fileIO.readInt())
+for _ in 0..<input[0] {
+    numbers.append(Int(readLine()!)!)
 }
 
 numbers.sort()
 
-var left = 0
-var right = 0
+var result = Int.max
+var start = 0
+var end = 0
 
-while right < numbers.count,
-      left < numbers.count {
-    let difference = numbers[right] - numbers[left]
-    if difference >= minimum {
+while start <= end,
+      end < input[0] {
+    let difference = numbers[end] - numbers[start]
+    
+    if difference >= input[1] {
         result = min(result, difference)
-        left += 1
+        start += 1
     } else {
-        right += 1
+        end += 1
     }
 }
 
