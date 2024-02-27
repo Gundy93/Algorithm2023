75184c2558785fcffe2f5e9570f587b776cfb0d3
GUNDY <106914201+Gundy93@users.noreply.github.com>
2024-02-27 13:36:12 +0900

    Update 1644_소수의 연속합.swift

--- Gundy/BAEKJOON/1644_소수의 연속합.swift
+++ Gundy/BAEKJOON/1644_소수의 연속합.swift
@@ -1,56 +1,38 @@
-import Foundation
-
-func primeNumbers(lessThanOrEqualTo target: Int) -> [Int] {
-    var result = [Int]()
-    
-    for number in stride(from: 2, through: target, by: 1) {
-        var isValid = true
-        
-        for divisor in result {
-            if number % divisor == 0 {
-                isValid = false
-                
-                break
-            }
-            
-            guard Double(divisor) <= sqrt(Double(number)) else { break }
-        }
-        
-        if isValid {
-            result.append(number)
-        }
-    }
-    
-    return result
-}
-
 let target = Int(readLine()!)!
-var result = 0
 
-if target > 1 {
-    let numbers = primeNumbers(lessThanOrEqualTo: target)
-    var left = 0
-    var right = 0
-    var partialSum = numbers[0]
+if target == 1 {
+    print(0)
+} else {
+    var isPrime = Array(repeating: true, count: target+1)
 
-    while left <= right {
-        if partialSum == target {
-            result += 1
-        }
-        
-        if partialSum <= target {
-            right += 1
-            
-            if right < numbers.count {
-                partialSum += numbers[right]
-            } else {
-                break
+    for number in 2...target {
+        if isPrime[number] {
+            for notPrimeNumber in stride(from: number * 2, through: target, by: number) {
+                isPrime[notPrimeNumber] = false
             }
-        } else {
-            partialSum -= numbers[left]
-            left += 1
         }
     }
-}
 
-print(result)
+    let primeNumbers = (2...target).filter { isPrime[$0] }
+    var partialSum = 2
+    var start = 0
+    var end = 0
+    var result = 0
+
+    loop: while start <= end {
+        switch partialSum {
+        case target:
+            result += 1
+            fallthrough
+        case ..<target:
+            end += 1
+            guard end < primeNumbers.count else { break loop }
+            partialSum += primeNumbers[end]
+        default:
+            partialSum -= primeNumbers[start]
+            start += 1
+        }
+    }
+
+    print(result)
+}
