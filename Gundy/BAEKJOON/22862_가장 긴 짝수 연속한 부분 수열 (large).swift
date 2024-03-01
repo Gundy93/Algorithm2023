0db9ba6932415aee33afd6597db4f5e014b8c931
GUNDY <106914201+Gundy93@users.noreply.github.com>
2024-03-01 13:49:41 +0900

    Update 22862_가장 긴 짝수 연속한 부분 수열 (large).swift

--- Gundy/BAEKJOON/22862_가장 긴 짝수 연속한 부분 수열 (large).swift
+++ Gundy/BAEKJOON/22862_가장 긴 짝수 연속한 부분 수열 (large).swift
@@ -1,24 +1,27 @@
-let input = readLine()!.split(separator: " ").compactMap({ Int($0) })
-let numbers = readLine()!.split(separator: " ").compactMap({ Int($0) })
+let input = readLine()!.split(separator: " ").map { Int($0)! }
+let numbers = readLine()!.split(separator: " ").map { Int($0)! }
 var start = 0
-var end = 0
-var count = numbers[0] % 2 == 1 ? 1 : 0
-var result = 1 - count
+var end = -1
+var count = 0
+var result = 0
 
-while end < input[0] - 1 {
+while end < input[0]-1 {
     end += 1
+    
     if numbers[end] % 2 == 1 {
         count += 1
-        while count > input[1] {
+        
+        while count > input[1],
+              start < input[0] {
             if numbers[start] % 2 == 1 {
                 count -= 1
             }
+            
             start += 1
         }
     }
-    if result < end - start + 1 - count {
-        result = end - start + 1 - count
-    }
+    
+    result = max(result, end - start + 1 - count)
 }
 
 print(result)
