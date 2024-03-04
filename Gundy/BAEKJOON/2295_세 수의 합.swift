1352cbbda3f55a975471f3ee28fa2ace3039d2cb
GUNDY <106914201+Gundy93@users.noreply.github.com>
2024-03-04 22:41:28 +0900

    Update 2295_세 수의 합.swift

--- Gundy/BAEKJOON/2295_세 수의 합.swift
+++ Gundy/BAEKJOON/2295_세 수의 합.swift
@@ -1,45 +1,27 @@
-var numbers = [Int]()
-
-for _ in 1...Int(readLine()!)! {
-    numbers.append(Int(readLine()!)!)
-}
-
-numbers.sort()
-
-var double = [Int]()
-
-for first in 0..<numbers.count - 1 {
-    for second in first..<numbers.count - 1 {
-        double.append(numbers[first] + numbers[second])
+func solution() {
+    let count = Int(readLine()!)!
+    var numbers = [Int]()
+    
+    for _ in 0..<count {
+        numbers.append(Int(readLine()!)!)
     }
-}
-
-double.sort()
-
-var result = 0
-
-for first in 0..<numbers.count {
-    for second in 0...first {
-        if firstIndex(of: numbers[first] - numbers[second]) != nil,
-           result < numbers[first] {
-            result = numbers[first]
+    
+    var sum = Set<Int>()
+    
+    for first in 0..<count {
+        for second in first..<count {
+            sum.insert(numbers[first] + numbers[second])
+        }
+    }
+    
+    for number in numbers.sorted(by: >) {
+        for third in numbers {
+            if sum.contains(number - third) {
+                print(number)
+                return
+            }
         }
     }
 }
 
-print(result)
-
-func firstIndex(of number: Int) -> Int? {
-    var start = 0
-    var end = double.count - 1
-    while start <= end {
-        let half = (start + end) / 2
-        if double[half] >= number {
-            end = half - 1
-        } else {
-            start = half + 1
-        }
-    }
-    guard start < double.count else { return nil }
-    return double[start] == number ? start : nil
-}
+solution()
