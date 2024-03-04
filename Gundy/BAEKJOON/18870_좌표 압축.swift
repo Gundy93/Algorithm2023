88644ee2b1d0ecb3da619efd75d5ab6eedf68ca9
GUNDY <106914201+Gundy93@users.noreply.github.com>
2024-03-04 22:27:40 +0900

    Update 18870_좌표 압축.swift

--- Gundy/BAEKJOON/18870_좌표 압축.swift
+++ Gundy/BAEKJOON/18870_좌표 압축.swift
@@ -1,9 +1,28 @@
+var cache = [Int : Int]()
 _ = readLine()
 let numbers = readLine()!.split(separator: " ").map { Int($0)! }
-var converter = [Int: Int]()
-
-for (index, number) in Set(numbers).sorted().enumerated() {
-    converter[number] = index
+let sorted = Set(numbers).sorted() 
+let result: [Int] = numbers.map { number in
+    if let cached = cache[number] {
+        return cached
+    }
+    
+    var start = 0
+    var end = sorted.count-1
+    
+    while start < end {
+        let mid = (start + end + 1) / 2
+        
+        if sorted[mid] <= number {
+            start = mid
+        } else {
+            end = mid - 1
+        }
+    }
+    
+    cache[number] = start
+    
+    return start
 }
 
-print(numbers.map { String(converter[$0]!) }.joined(separator: " "))
+print(result.map(String.init).joined(separator: " "))
