e622603e1e1774a918aa9005fe976af71298c033
GUNDY <106914201+Gundy93@users.noreply.github.com>
2024-03-06 13:28:36 +0900

    Update 정수를 나선형으로 배치하기.swift

--- Gundy/Programmers/LV. 0/정수를 나선형으로 배치하기.swift
+++ Gundy/Programmers/LV. 0/정수를 나선형으로 배치하기.swift
@@ -1,50 +1,68 @@
+import Foundation
+
+enum Direction {
+    case right
+    case down
+    case left
+    case up
+    
+    var next: Direction {
+        switch self {
+        case .right:
+            return .down
+        case .down:
+            return .left
+        case .left:
+            return .up
+        case .up:
+            return .right
+        }
+    }
+}
+
 func solution(_ n:Int) -> [[Int]] {
-    var result = Array(repeating: Array(repeating: 0, count: n), count: n)
+    var numbers = Array(repeating: Array(repeating: 0, count: n), count: n)
     var row = 0
     var column = 0
     var direction = Direction.right
-    for number in 1...n * n {
-        result[row][column] = number
-        guard number < n * n else { break }
-    direction: while true {
-            switch direction {
-            case .up:
-                guard row - 1 >= 0, result[row - 1][column] == 0 else {
-                    direction = .right
-                    continue
-                }
-                row -= 1
-                break direction
-            case .down:
-                guard row + 1 < n, result[row + 1][column] == 0 else {
-                    direction = .left
-                    continue
-                }
-                row += 1
-                break direction
-            case .left:
-                guard column - 1 >= 0, result[row][column - 1] == 0 else {
-                    direction = .up
-                    continue
-                }
-                column -= 1
-                break direction
-            case .right:
-                guard column + 1 < n, result[row][column + 1] == 0 else {
-                    direction = .down
-                    continue
-                }
+    var number = 1
+    
+    while true {
+        numbers[row][column] = number
+        
+        guard number < n*n else { break }
+        
+        switch direction {
+        case .right:
+            if column+1 < n, numbers[row][column+1] == 0 {
                 column += 1
-                break direction
+                number += 1
+            } else {
+                direction = direction.next
+            }
+        case .down:
+            if row+1 < n, numbers[row+1][column] == 0 {
+                row += 1
+                number += 1
+            } else {
+                direction = direction.next
+            }
+        case .left:
+            if column-1 >= 0, numbers[row][column-1] == 0 {
+                column -= 1
+                number += 1
+            } else {
+                direction = direction.next
+            }
+        case .up:
+            if row-1 >= 0, numbers[row-1][column] == 0 {
+                row -= 1
+                number += 1
+            } else {
+                direction = direction.next
             }
         }
     }
-    return result
-}
-
-enum Direction {
-    case up
-    case down
-    case left
-    case right
+    
+    return numbers
 }
