6f35db58437da84982014ec23d518139c7fbade8
GUNDY <106914201+Gundy93@users.noreply.github.com>
2024-03-14 22:01:06 +0900

    Update 1644_소수의 연속합.swift

--- Gundy/BAEKJOON/1644_소수의 연속합.swift
+++ Gundy/BAEKJOON/1644_소수의 연속합.swift
@@ -1,38 +1,41 @@
 let target = Int(readLine()!)!
+var result = 0
 
-if target == 1 {
-    print(0)
-} else {
+if target >= 2 {
+    var primes = [Int]()
     var isPrime = Array(repeating: true, count: target+1)
-
+    
     for number in 2...target {
         if isPrime[number] {
-            for notPrimeNumber in stride(from: number * 2, through: target, by: number) {
-                isPrime[notPrimeNumber] = false
+            primes.append(number)
+            
+            for multiple in stride(from: number*2, through: target, by: number) {
+                isPrime[multiple] = false
             }
         }
     }
-
-    let primeNumbers = (2...target).filter { isPrime[$0] }
-    var partialSum = 2
+    
     var start = 0
     var end = 0
-    var result = 0
-
-    loop: while start <= end {
-        switch partialSum {
+    var sum = 2
+    
+    while end < primes.count,
+          start <= end {
+        switch sum {
+        case ..<target:
+            end += 1
+            
+            guard end < primes.count else { break }
+            
+            sum += primes[end]
         case target:
             result += 1
             fallthrough
-        case ..<target:
-            end += 1
-            guard end < primeNumbers.count else { break loop }
-            partialSum += primeNumbers[end]
         default:
-            partialSum -= primeNumbers[start]
+            sum -= primes[start]
             start += 1
         }
     }
-
-    print(result)
 }
+
+print(result)
