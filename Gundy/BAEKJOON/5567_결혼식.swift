cbedf31edee38e7a7eeaa5e1e8578d47cdae4a01
GUNDY <106914201+Gundy93@users.noreply.github.com>
2024-03-15 13:52:02 +0900

    Update 5567_결혼식.swift

--- Gundy/BAEKJOON/5567_결혼식.swift
+++ Gundy/BAEKJOON/5567_결혼식.swift
@@ -1,21 +1,27 @@
-var friendList = [Int: [Int]]()
-let numberOfFriends = Int(readLine()!)!
+_ = readLine()
+let count = Int(readLine()!)!
+var friends = [Int : [Int]]()
 
-for _ in 1...Int(readLine()!)! {
-    let friends = readLine()!.split(separator: " ").compactMap { Int($0) }
-    friendList[friends[0], default: []].append(friends[1])
-    friendList[friends[1], default: []].append(friends[0])
-}
-
-var guests = Set<Int>()
-
-if let friends = friendList[1] {
-    for friend in friends {
-        guests.insert(friend)
-        guests = guests.union(friendList[friend]!)
-    }
+for _ in 0..<count {
+    let people = readLine()!.split(separator: " ").map { Int($0)! }
     
-    guests.remove(1)
+    friends[people[0], default: []].append(people[1])
+    friends[people[1], default: []].append(people[0])
 }
 
-print(guests.count)
+var visited = Set<Int>()
+var people = [(1, 0)]
+var index = 0
+
+while index < people.count {
+    let (friend, distance) = people[index]
+    index += 1
+    
+    guard visited.contains(friend) == false,
+          distance <= 2 else { continue }
+    
+    visited.insert(friend)
+    people += friends[friend, default: []].map { ($0, distance+1) }
+}
+
+print(visited.count-1)
