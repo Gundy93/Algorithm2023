74fa1d991b99049b19bff05779a3a3919f4a810d
GUNDY <106914201+Gundy93@users.noreply.github.com>
2024-03-21 16:25:33 +0900

    Update 당구 연습.swift

--- Gundy/Programmers/LV. 2/당구 연습.swift
+++ Gundy/Programmers/LV. 2/당구 연습.swift
@@ -1,28 +1,40 @@
+import Foundation
+
 func solution(_ m:Int, _ n:Int, _ startX:Int, _ startY:Int, _ balls:[[Int]]) -> [Int] {
     var result = [Int]()
+    
     for ball in balls {
-        var range = Int.max
-        let UDWidth = abs(startX - ball[0])
-        let upHeight = 2 * n - startY - ball[1]
-        let upRange = UDWidth * UDWidth + upHeight * upHeight
-        let downHeight = startY + ball[1]
-        let downRange = UDWidth * UDWidth + downHeight * downHeight
-        if UDWidth == 0 {
-            range = startY - ball[1] > 0 ? upRange: downRange
-        } else {
-            range = min(upRange, downRange)
+        var distance = Int.max
+        if startX != ball[0] || startY > ball[1] {
+            let height = n - startY + n - ball[1]
+            let width = abs(startX - ball[0])
+            
+            distance = min(distance, height * height + width * width)
         }
-        let LRHeight = abs(startY - ball[1])
-        let leftWidht = startX + ball[0]
-        let leftRange = LRHeight * LRHeight + leftWidht * leftWidht
-        let rightWidth = 2 * m - startX - ball[0]
-        let rightRange = LRHeight * LRHeight + rightWidth * rightWidth
-        if LRHeight == 0 {
-            range = startX - ball[0] < 0 ? min(range, leftRange): min(range, rightRange)
-        } else {
-            range = min(range, min(leftRange, rightRange))
+        
+        if startX != ball[0] || startY < ball[1] {
+            let height = startY + ball[1]
+            let width = abs(startX - ball[0])
+            
+            distance = min(distance, height * height + width * width)
         }
-        result.append(range)
+        
+        if startY != ball[1] || startX < ball[0] {
+            let height = abs(startY - ball[1])
+            let width = startX + ball[0]
+            
+            distance = min(distance, height * height + width * width)
+        }
+        
+        if startY != ball[1] || startX > ball[0] {
+            let height = abs(startY - ball[1])
+            let width = m - startX + m - ball[0]
+            
+            distance = min(distance, height * height + width * width)
+        }
+        
+        result.append(distance)
     }
+    
     return result
 }
