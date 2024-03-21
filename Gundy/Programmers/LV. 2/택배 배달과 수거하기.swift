0e8e6dba4966dca204d97a9aaa8868d73a27aaef
GUNDY <106914201+Gundy93@users.noreply.github.com>
2024-03-21 20:53:08 +0900

    Update 택배 배달과 수거하기.swift

--- Gundy/Programmers/LV. 2/택배 배달과 수거하기.swift
+++ Gundy/Programmers/LV. 2/택배 배달과 수거하기.swift
@@ -2,31 +2,35 @@ func solution(_ cap:Int, _ n:Int, _ deliveries:[Int], _ pickups:[Int]) -> Int64 
     var result = 0
     var deliveries = deliveries
     var pickups = pickups
+    
     while deliveries.last == 0 {
         deliveries.removeLast()
     }
+    
     while pickups.last == 0 {
         pickups.removeLast()
     }
+    
     while deliveries.isEmpty == false || pickups.isEmpty == false {
         result += max(deliveries.count, pickups.count) * 2
-        var serving = cap
-        var returning = 0
-        while deliveries.isEmpty == false {
-            if deliveries.last! > serving {
-                deliveries[deliveries.count - 1] -= serving
+        
+        var delivery = cap
+        var pickup = cap
+        while let last = deliveries.last {
+            if last > delivery {
+                deliveries[deliveries.count - 1] -= delivery
                 break
             } else {
-                serving -= deliveries.last!
+                delivery -= last
                 deliveries.removeLast()
             }
         }
-        while pickups.isEmpty == false {
-            if pickups.last! > cap - returning {
-                pickups[pickups.count - 1] -= cap - returning
+        while let last = pickups.last {
+            if last > pickup {
+                pickups[pickups.count - 1] -= pickup
                 break
             } else {
-                returning += pickups.last!
+                pickup -= last
                 pickups.removeLast()
             }
         }
