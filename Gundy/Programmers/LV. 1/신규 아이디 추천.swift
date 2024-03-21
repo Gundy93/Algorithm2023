ed6617d73e7cc63f801bd7ba45419576534d2e87
GUNDY <106914201+Gundy93@users.noreply.github.com>
2024-03-21 10:12:01 +0900

    Update 신규 아이디 추천.swift

--- Gundy/Programmers/LV. 1/신규 아이디 추천.swift
+++ Gundy/Programmers/LV. 1/신규 아이디 추천.swift
@@ -1,40 +1,28 @@
+import Foundation
+
 func solution(_ new_id:String) -> String {
-    let id: String = new_id.lowercased()
-    var filteredID: String = ""
-    for text in id {
-        let word: String = String(text)
-        if text.isLowercase {
-            filteredID += word
-        } else if let number = Int(word) {
-            filteredID += word
-        } else if text == "-" ||
-                    text == "_" {
-            filteredID += word
-        } else if word == ".",
-                  filteredID.isEmpty == false,
-                  text != filteredID.last {
-            filteredID += word
+    var newID = new_id.lowercased()
+    newID = newID.filter { $0.isCased || $0.isNumber || ["-", "_", "."].contains($0) }
+    while newID.contains("..") {
+        newID = newID.replacingOccurrences(of: "..", with: ".")
+    }
+    while newID.first == "." {
+        newID = String(newID.dropFirst())
+    }
+    while newID.last == "." {
+        newID.popLast()
+    }
+    if newID.isEmpty {
+        newID = "a"
+    }
+    if newID.count > 15 {
+        newID = String(newID.dropLast(newID.count-15))
+        while newID.last == "." {
+            newID.popLast()
         }
     }
-    if filteredID.last == "." {
-        filteredID.removeLast()
+    while newID.count < 3 {
+        newID += String(newID.last!)
     }
-    if filteredID.isEmpty {
-        filteredID = "a"
-    }
-    if filteredID.count > 15 {
-        filteredID = String(filteredID.reversed())
-        var ID: String = ""
-        while ID.count < 15 {
-            ID += String(filteredID.removeLast())
-        }
-        while ID.last == "." {
-            ID.removeLast()
-        }
-        filteredID = ID
-    }
-    while filteredID.count < 3 {
-        filteredID += String(filteredID.last!)
-    }
-    return filteredID
+    return newID
 }
