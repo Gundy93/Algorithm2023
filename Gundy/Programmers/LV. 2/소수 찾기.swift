c4505abac4650ff7053eff1c14f234350372f557
GUNDY <106914201+Gundy93@users.noreply.github.com>
2024-03-22 14:57:50 +0900

    Update 소수 찾기.swift

--- Gundy/Programmers/LV. 2/소수 찾기.swift
+++ Gundy/Programmers/LV. 2/소수 찾기.swift
@@ -1,40 +1,40 @@
 import Foundation
 
 func solution(_ numbers:String) -> Int {
+    let maxNumber = String(numbers.sorted(by: >))
+    var isPrime = Array(repeating: true, count: Int(maxNumber)!+1)
+    
+    for number in 2..<isPrime.count {
+        if isPrime[number] {
+            for multiple in stride(from: number * number, to: isPrime.count, by: number) {
+                isPrime[multiple] = false
+            }
+        }
+    }
+    
+    isPrime[0] = false
+    isPrime[1] = false
+    
     let numbers = numbers.map(String.init)
-    var joinedNumbers = Set<Int>()
-    var result = 0
-    func recursion(_ number: String, indices: Set<Int>) {
-        guard indices.count < numbers.count else {
-            return
+    var result = Set<Int>()
+    
+    func recursion(_ indices: Set<Int>, _ number: String) {
+        if let number = Int(number),
+           isPrime[number] {
+            result.insert(number)
         }
-        for index in 0..<numbers.count {
-            guard indices.contains(index) == false else {
-                continue
-            }
-            let number = number + numbers[index]
-            joinedNumbers.insert(Int(number)!)
-            recursion(number, indices: indices.union([index]))
-        }
-    }
-    recursion("", indices: [])
-    func validate(_ number: Int) -> Bool {
-        guard number > 3 else {
-            return number > 1 ? true : false
-        }
-        var divisor = 2
-        while divisor <= Int(sqrt(Double(number))) {
-            guard number % divisor != 0 else {
-                return false
-            }
-            divisor += 1
-        }
-        return true
-    }
-    for number in joinedNumbers {
-        if validate(number) {
-            result += 1
+        
+        if indices.count == numbers.count { return }
+        
+        for index in 0..<numbers.count where indices.contains(index) == false {
+            var indices = indices
+            
+            indices.insert(index)
+            recursion(indices, number + numbers[index])
         }
     }
-    return result
+    
+    recursion([], "")
+    
+    return result.count
 }
