e7662fd2beb6f14bfa6f4b83c0ee374c352c3f08
GUNDY <106914201+Gundy93@users.noreply.github.com>
2024-03-25 20:45:50 +0900

    Update 1991_트리 순회.swift

--- Gundy/BAEKJOON/1991_트리 순회.swift
+++ Gundy/BAEKJOON/1991_트리 순회.swift
@@ -1,50 +1,32 @@
-let number = Int(readLine()!)!
-var links = [String: (String?, String?)]()
-for _ in 1...number {
-    let nodes = readLine()!.split(separator: " ").map(String.init)
-    links[nodes[0], default: (nil, nil)].0 = nodes[1] == "." ? nil: nodes[1]
-    links[nodes[0], default: (nil, nil)].1 = nodes[2] == "." ? nil: nodes[2]
+func solution() {
+    let count = Int(readLine()!)!
+    var childs = [String : (String, String)]()
+    
+    for _ in 0..<count {
+        let nodes = readLine()!.split(separator: " ").map(String.init)
+        
+        childs[nodes[0]] = (nodes[1], nodes[2])
+    }
+    
+    func preorder(_ node: String) -> String {
+        guard node != "." else { return "" }
+        
+        return node + preorder(childs[node]!.0) + preorder(childs[node]!.1)
+    }
+    
+    func inorder(_ node: String) -> String {
+        guard node != "." else { return "" }
+        
+        return inorder(childs[node]!.0) + node + inorder(childs[node]!.1)
+    }
+    
+    func postorder(_ node: String) -> String {
+        guard node != "." else { return "" }
+        
+        return postorder(childs[node]!.0) + postorder(childs[node]!.1) + node
+    }
+    
+    print(preorder("A"), inorder("A"), postorder("A"), separator: "\n")
 }
 
-func preorder(_ node: String) -> String {
-    var result = String()
-    result += node
-    let childs = links[node]!
-    if let left = childs.0 {
-        result += preorder(left)
-    }
-    if let right = childs.1 {
-        result += preorder(right)
-    }
-    return result
-}
-
-func inorder(_ node: String) -> String {
-    var result = String()
-    let childs = links[node]!
-    if let left = childs.0 {
-        result += inorder(left)
-    }
-    result += node
-    if let right = childs.1 {
-        result += inorder(right)
-    }
-    return result
-}
-
-func postorder(_ node: String) -> String {
-    var result = String()
-    let childs = links[node]!
-    if let left = childs.0 {
-        result += postorder(left)
-    }
-    if let right = childs.1 {
-        result += postorder(right)
-    }
-    result += node
-    return result
-}
-
-print(preorder("A"))
-print(inorder("A"))
-print(postorder("A"))
+solution()
