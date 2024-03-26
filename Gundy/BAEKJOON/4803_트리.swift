04b9216d67bc03ad64a2ffc5e82b17a773890792
GUNDY <106914201+Gundy93@users.noreply.github.com>
2024-03-26 13:42:36 +0900

    Update 4803_트리.swift

--- Gundy/BAEKJOON/4803_트리.swift
+++ Gundy/BAEKJOON/4803_트리.swift
@@ -1,46 +1,68 @@
-var input = readLine()!
-var numberOfCases = 1
-
-while input != "0 0" {
-    let command = input.split(separator: " ").map(String.init).compactMap(Int.init)
-    var numberOfTrees = 0
-    var links = [Int: [Int]]()
-    for _ in stride(from: 1, through: command[1], by: 1) {
-        let nodes = readLine()!.split(separator: " ").map(String.init).compactMap(Int.init)	
-        links[nodes[0], default: []].append(nodes[1])
-        links[nodes[1], default: []].append(nodes[0])
-    }
-    var visited = Set<Int>()
-    for node in 1...command[0] {
-        guard visited.contains(node) == false else { continue }
-        var isValid = true
-        var needVisit: [(Int, Int?)] = [(node, nil)]
-        while needVisit.isEmpty == false {
-            let now = needVisit.removeLast()
-            guard visited.contains(now.0) == false else { continue }
-            visited.insert(now.0)
-            if let nodes = links[now.0] {
-                for node in nodes {
-                    guard node != now.1 else { continue }
-                    if visited.contains(node) {
-                        isValid = false
+func solution() {
+    func findTrees(_ caseNumber: Int) -> String? {
+        guard let input = readLine(),
+              input != "0 0" else { return nil }
+        
+        let counts = input.split(separator: " ").map { Int($0)! }
+        var links = [Int : [Int]]()
+        
+        for _ in 0..<counts[1] {
+            let link = readLine()!.split(separator: " ").map { Int($0)! }
+            
+            links[link[0], default: []].append(link[1])
+            links[link[1], default: []].append(link[0])
+        }
+        
+        var visited = Set<Int>()
+        var count = 0
+        
+        for start in 1...counts[0] {
+            guard visited.contains(start) == false else { continue }
+            
+            var isCycle = false
+            var needVisit = [(start, start)]
+            
+            while let (current, previous) = needVisit.popLast() {
+                guard visited.contains(current) == false else { continue }
+                
+                visited.insert(current)
+                
+                for next in links[current, default: []] where next != previous {
+                    guard visited.contains(next) == false else {
+                        isCycle = true
+                        continue
                     }
-                    needVisit.append((node, now.0))
+                    
+                    needVisit.append((next, current))
                 }
             }
+            
+            if isCycle == false {
+                count += 1
+            }
         }
-        if isValid {
-            numberOfTrees += 1
+        
+        switch count {
+        case 0:
+            return "Case " + String(caseNumber) + ": No trees.\n"
+        case 1:
+            return "Case " + String(caseNumber) + ": There is one tree.\n"
+        case let count:
+            return "Case " + String(caseNumber) + ": A forest of " + String(count) + " trees.\n"
         }
     }
-    switch numberOfTrees {
-    case 0:
-        print("Case \(numberOfCases): No trees.")
-    case 1:
-        print("Case \(numberOfCases): There is one tree.")
-    default:
-        print("Case \(numberOfCases): A forest of \(numberOfTrees) trees.")
+    
+    var result = ""
+    
+    for caseNumber in 1... {
+        if let text = findTrees(caseNumber) {
+            result += text
+        } else {
+            break
+        }
     }
-    numberOfCases += 1
-    input = readLine()!
+    
+    print(result)
 }
+
+solution()
