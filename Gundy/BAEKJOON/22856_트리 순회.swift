7c1618a0a0716e11f705c3546a959c6bc31cfc23
GUNDY <106914201+Gundy93@users.noreply.github.com>
2024-03-29 13:49:11 +0900

    Update 22856_트리 순회.swift

--- Gundy/BAEKJOON/22856_트리 순회.swift
+++ Gundy/BAEKJOON/22856_트리 순회.swift
@@ -1,30 +1,36 @@
-let numberOfNodes = Int(readLine()!)!
-var nodes: [(Int?, Int?)] = Array(repeating: (nil, nil), count: numberOfNodes + 1)
-for _ in 1...numberOfNodes {
-    let node = readLine()!.split(separator: " ").map(String.init).compactMap(Int.init)
-    nodes[node[0]].0 = node[1] == -1 ? nil : node[1]
-    nodes[node[0]].1 = node[2] == -1 ? nil : node[2]
-}
-var result = 0
-func recursion(_ node: Int, _ isLeft: Bool) {
-    if nodes[node].0 == nil, nodes[node].1 == nil {
-        return
+func solution() {
+    let numberOfNodes = Int(readLine()!)!
+    var nodes = Array(
+        repeating: (-1, -1),
+        count: numberOfNodes+1
+    )
+    
+    for _ in 0..<numberOfNodes {
+        let node = readLine()!.split(separator: " ").map { Int($0)! }
+        
+        nodes[node[0]] = (node[1], node[2])
     }
-    if let left = nodes[node].0 {
-        recursion(left, isLeft)
-        result += 2
-    }
-    if let right = nodes[node].1 {
-        recursion(right, isLeft)
+    
+    var result = -1
+    
+    func similarInorder(_ node: Int, isLeft: Bool) {
+        if nodes[node].0 != -1 {
+            similarInorder(nodes[node].0, isLeft: true)
+        }
+        
+        if nodes[node].1 != -1 {
+            similarInorder(nodes[node].1, isLeft: isLeft)
+        }
+        
         result += isLeft ? 2 : 1
     }
+    
+    similarInorder(
+        1,
+        isLeft: false
+    )
+    
+    print(result)
 }
-if let left = nodes[1].0 {
-    recursion(left, true)
-    result += 2
-}
-if let right = nodes[1].1 {
-    recursion(right, false)
-    result += 1
-}
-print(result)
+
+solution()
