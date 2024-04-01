3de7de3d102ec4c45494261d4283190597d9a154
GUNDY <106914201+Gundy93@users.noreply.github.com>
2024-04-01 11:34:12 +0900

    Update 멀쩡한 사각형.swift

--- Gundy/Programmers/LV. 2/멀쩡한 사각형.swift
+++ Gundy/Programmers/LV. 2/멀쩡한 사각형.swift
@@ -1,8 +1,11 @@
-func solution(_ w:Int, _ h:Int) -> Int64{
-    return Int64(w * h - w - h + getGCD(w, h))
-}
+import Foundation
 
-func getGCD(_ a: Int, _ b: Int) -> Int {
-    guard b != 0 else { return a }
-    return getGCD(b, a % b)
+func solution(_ w:Int, _ h:Int) -> Int64 {
+    var result = 0
+    
+    for row in 1...h {
+        result += (h - row) * w / h
+    }
+
+    return Int64(result*2)
 }
