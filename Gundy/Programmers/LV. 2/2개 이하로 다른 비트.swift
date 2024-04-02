052669850f8dae44f4cb05237ab231a2217787b6
GUNDY <106914201+Gundy93@users.noreply.github.com>
2024-04-02 11:03:56 +0900

    Update 2개 이하로 다른 비트.swift

--- Gundy/Programmers/LV. 2/2개 이하로 다른 비트.swift
+++ Gundy/Programmers/LV. 2/2개 이하로 다른 비트.swift
@@ -1,21 +1,18 @@
+import Foundation
+
 func solution(_ numbers:[Int64]) -> [Int64] {
-    var result: [Int64] = []
-    for number in numbers {
-        if number % 2 == 0 {
-            result.append(number + 1)
-        } else {
-            result.append(makeNext(for: number))
-        }
-    }
-    return result
+    return numbers.map(f)
 }
 
-func makeNext(for number: Int64) -> Int64 {
-    var binary: String = String(number, radix: 2)
-    var value: Int = 1
-    while binary.last == "1" {
-        value *= 2
-        binary.removeLast()
+func f(_ x: Int64) -> Int64 {
+    let numbers = Array(String(x, radix: 2))
+    var index = numbers.count-1
+    
+    while index >= 0 {
+        if numbers[index] == "0" { break }
+        
+        index -= 1
     }
-    return number + Int64(value / 2)
+    
+    return x + (Array(repeating: Int64(2), count: numbers.count-1 - index).reduce(1, *)+1)/2
 }
