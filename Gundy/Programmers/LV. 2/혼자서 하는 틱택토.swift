a0ce0972f7e28794af52897ced6d115f4c81d521
GUNDY <106914201+Gundy93@users.noreply.github.com>
2024-04-03 11:56:39 +0900

    Update 혼자서 하는 틱택토.swift

--- Gundy/Programmers/LV. 2/혼자서 하는 틱택토.swift
+++ Gundy/Programmers/LV. 2/혼자서 하는 틱택토.swift
@@ -1,73 +1,64 @@
+import Foundation
+
 func solution(_ board:[String]) -> Int {
     let board = board.map { $0.map(String.init) }
-    var oWidth = Set<Int>()
-    var oHeight = Set<Int>()
-    var oDiagonal = Set<Int>()
-    var oCount = 0
-    var isOWin = false
-    var xWidth = Set<Int>()
-    var xHeight = Set<Int>()
-    var xDiagonal = Set<Int>()
-    var xCount = 0
-    var isXWin = false
-    var diagonal = [[[0, 0], [1, 1], [2, 2]], [[0, 2], [1, 1], [2, 0]]]
+    var count = [0, 0]
+    var rows = Array(repeating: [0, 0], count: 3)
+    var columns = Array(repeating: [0, 0], count: 3)
+    var cross = Array(repeating: [0, 0], count: 2)
+    
     for row in 0...2 {
-        for column in 0...2 {
-            if board[row][column] == "O" {
-                oCount += 1
-                if isOWin == false, oWidth.contains(column) == false {
-                    oWidth.insert(row)
-                    if board[row][0] == board[row][1], board[row][1] == board[row][2] {
-                        isOWin = true
-                    }
-                }
-                if isOWin == false, oHeight.contains(row) == false {
-                    oWidth.insert(row)
-                    if board[0][column] == board[1][column], board[1][column] == board[2][column] {
-                        isOWin = true
-                    }
-                }
-                for index in 0...1 {
-                    if isOWin == false, diagonal[index].contains([row, column]), oDiagonal.contains(index) == false {
-                        oDiagonal.insert(index)
-                        if board[diagonal[index][0][0]][diagonal[index][0][1]] == board[diagonal[index][1][0]][diagonal[index][1][1]], board[diagonal[index][1][0]][diagonal[index][1][1]] == board[diagonal[index][2][0]][diagonal[index][2][1]] {
-                            isOWin = true
-                        }
-                    }
-                }
-            } else if board[row][column] == "X" {
-                xCount += 1
-                if isXWin == false, xWidth.contains(column) == false {
-                    xWidth.insert(row)
-                    if board[row][0] == board[row][1], board[row][1] == board[row][2] {
-                        isXWin = true
-                    }
-                }
-                if isXWin == false, xHeight.contains(row) == false {
-                    xWidth.insert(row)
-                    if board[0][column] == board[1][column], board[1][column] == board[2][column] {
-                        isXWin = true
-                    }
-                }
-                for index in 0...1 {
-                    if isXWin == false, diagonal[index].contains([row, column]), oDiagonal.contains(index) == false {
-                        xDiagonal.insert(index)
-                        if board[diagonal[index][0][0]][diagonal[index][0][1]] == board[diagonal[index][1][0]][diagonal[index][1][1]], board[diagonal[index][1][0]][diagonal[index][1][1]] == board[diagonal[index][2][0]][diagonal[index][2][1]] {
-                            isXWin = true
-                        }
-                    }
-                }
+        for column in 0...2 where board[row][column] != "." {
+            let index = board[row][column] == "O" ? 0 : 1
+            
+            count[index] += 1
+            rows[row][index] += 1
+            columns[column][index] += 1
+            
+            if row == 1,
+               column == 1 {
+                cross[0][index] += 1
+                cross[1][index] += 1
+            } else if [0, 2].contains(row),
+                      [0, 2].contains(column) {
+                cross[row == column ? 0 : 1][index] += 1
             }
         }
     }
-    switch (isOWin, isXWin) {
+    
+    var oWin = false
+    var xWin = false
+    
+    rows.forEach {
+        if $0[0] == 3 {
+            oWin = true
+        } else if $0[1] == 3 {
+            xWin = true
+        }
+    }
+    columns.forEach {
+        if $0[0] == 3 {
+            oWin = true
+        } else if $0[1] == 3 {
+            xWin = true
+        }
+    }
+    cross.forEach {
+        if $0[0] == 3 {
+            oWin = true
+        } else if $0[1] == 3 {
+            xWin = true
+        }
+    }
+    
+    switch (oWin, xWin) {
     case (true, true):
         return 0
     case (true, false):
-        return oCount == xCount + 1 ? 1 : 0
+        return count[0] == count[1]+1 ? 1 : 0
     case (false, true):
-        return oCount == xCount ? 1 : 0
+        return count[0] == count[1] ? 1 : 0
     default:
-        return oCount == xCount + 1 || oCount == xCount ? 1 : 0
+        return (count[0] >= count[1] && count[1]+1 >= count[0]) ? 1 : 0
     }
 }
