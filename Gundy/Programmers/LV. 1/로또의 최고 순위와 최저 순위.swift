540aadf7812bc4f2bb8daddf93ec6fbf1bffa43c
GUNDY <106914201+Gundy93@users.noreply.github.com>
2024-04-04 14:59:37 +0900

    Update 로또의 최고 순위와 최저 순위.swift

--- Gundy/Programmers/LV. 1/로또의 최고 순위와 최저 순위.swift
+++ Gundy/Programmers/LV. 1/로또의 최고 순위와 최저 순위.swift
@@ -1,16 +1,7 @@
+import Foundation
+
 func solution(_ lottos:[Int], _ win_nums:[Int]) -> [Int] {
-    let winningNumbers: Set<Int> = Set(win_nums)
-    var zeroCount: Int = 0
-    var rank: Int = 7
-    for number in lottos {
-        if number == 0 {
-            zeroCount += 1
-        } else {
-            if winningNumbers.contains(number) {
-                rank -= 1
-            }
-        }
-    }
-    let maximumRank: Int = rank - zeroCount
-    return [maximumRank == 7 ? 6 : maximumRank, rank == 7 ? 6 : rank]
+    let correctCount = Set(win_nums).intersection(lottos).count
+    let zeroCount = lottos.filter { $0 == 0 }.count
+    return [min(7 - correctCount - zeroCount, 6), min(7 - correctCount, 6)]
 }
