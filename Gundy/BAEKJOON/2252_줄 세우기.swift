212b6e2cb40fae131854e8e22cde8ea4f43062eb
GUNDY <106914201+Gundy93@users.noreply.github.com>
2024-04-05 12:34:05 +0900

    Update 2252_줄 세우기.swift

--- Gundy/BAEKJOON/2252_줄 세우기.swift
+++ Gundy/BAEKJOON/2252_줄 세우기.swift
@@ -1,25 +1,35 @@
-let input = readLine()!.split(separator: " ").map(String.init).compactMap(Int.init)
-var students = [Int: (Set<Int>, Set<Int>)]()
-for _ in 1...input[1] {
-    let numbers = readLine()!.split(separator: " ").map(String.init).compactMap(Int.init)
-    students[numbers[0], default: ([], [])].0.insert(numbers[1])
-    students[numbers[1], default: ([], [])].1.insert(numbers[0])
-}
-var zeros = [Int]()
-var result = String()
-for number in 1...input[0] {
-    if students[number, default: ([], [])].1.isEmpty {
-        zeros.append(number)
+func solution() {
+    let input = readLine()!.split(separator: " ").map { Int($0)! }
+    var previousCount = Array(repeating: 0, count: input[0]+1)
+    var next = [Int : [Int]]()
+    
+    for _ in 0..<input[1] {
+        let students = readLine()!.split(separator: " ").map { Int($0)! }
+        previousCount[students[1]] += 1
+        next[students[0], default: []].append(students[1])
     }
-}
-while zeros.isEmpty == false {
-    let last = zeros.removeLast()
-    result += result.isEmpty ? String(last) : " \(last)"
-    for target in students[last, default: ([], [])].0 {
-        students[target, default: ([], [])].1.remove(last)
-        if students[target, default: ([], [])].1.isEmpty {
-            zeros.append(target)
+    
+    var students = [Int]()
+    
+    for student in 1...input[0] where previousCount[student] == 0 {
+        students.append(student)
+    }
+    
+    var result = [String]()
+    
+    while let smallStudent = students.popLast() {
+        result.append(String(smallStudent))
+        
+        for next in next[smallStudent, default: []] {
+            previousCount[next] -= 1
+            
+            if previousCount[next] == 0 {
+                students.append(next)
+            }
         }
     }
+    
+    print(result.joined(separator: " "))
 }
-print(result)
+
+solution()
