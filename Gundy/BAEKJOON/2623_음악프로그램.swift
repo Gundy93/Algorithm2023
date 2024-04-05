881c264fa383fb9659191432e0cdb772021140e2
GUNDY <106914201+Gundy93@users.noreply.github.com>
2024-04-05 13:33:06 +0900

    Update 2623_음악프로그램.swift

--- Gundy/BAEKJOON/2623_음악프로그램.swift
+++ Gundy/BAEKJOON/2623_음악프로그램.swift
@@ -1,33 +1,34 @@
-let input = readLine()!.split(separator: " ").map(String.init).compactMap(Int.init)
-var singerList = [Int: (Set<Int>, Set<Int>)]()
-for _ in 1...input[1] {
-    let singers = readLine()!.split(separator: " ").map(String.init).compactMap(Int.init)
-    let numberOfSingers = singers[0]
-    guard numberOfSingers > 1 else { continue }
-    for index in 1...numberOfSingers - 1 {
-        singerList[singers[index], default: ([], [])].0.insert(singers[index + 1])
-        singerList[singers[index + 1], default: ([], [])].1.insert(singers[index])
+let input = readLine()!.split(separator: " ").map { Int($0)! }
+var previous = Array(repeating: Set<Int>(), count: input[0]+1)
+var next = [Int : Set<Int>]()
+
+for _ in 0..<input[1] {
+    let singers = readLine()!.split(separator: " ").map { Int($0)! }
+    
+    for index in 1..<singers.count-1 {
+        previous[singers[index+1]].insert(singers[index])
+        next[singers[index], default: []].insert(singers[index+1])
     }
 }
-var result = [Int]()
-var zeros = [Int]()
-for number in 1...input[0] {
-    if singerList[number, default: ([], [])].1.isEmpty {
-        zeros.append(number)
-    }
+
+var singers = [Int]()
+
+for singer in 1...input[0] where previous[singer].isEmpty {
+    singers.append(singer)
 }
-while zeros.isEmpty == false {
-    let last = zeros.removeLast()
-    result.append(last)
-    for number in singerList[last, default: ([], [])].0 {
-        singerList[number, default: ([], [])].1.remove(last)
-        if singerList[number, default: ([], [])].1.isEmpty {
-            zeros.append(number)
+
+var result = [String]()
+
+while let singer = singers.popLast() {
+    result.append(String(singer))
+    
+    for next in next[singer, default: []] {
+        previous[next].remove(singer)
+        
+        if previous[next].isEmpty {
+            singers.append(next)
         }
     }
 }
-if result.count == input[0] {
-    result.forEach({ print($0) })
-} else {
-    print(0)
-}
+
+print(result.count == input[0] ? result.joined(separator: "\n") : 0)
