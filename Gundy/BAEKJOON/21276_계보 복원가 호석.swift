8e84c45387ddbad2f3ea99497cd88a2a265ab7e0
GUNDY <106914201+Gundy93@users.noreply.github.com>
2024-04-05 15:39:04 +0900

    Update 21276_계보 복원가 호석.swift

--- Gundy/BAEKJOON/21276_계보 복원가 호석.swift
+++ Gundy/BAEKJOON/21276_계보 복원가 호석.swift
@@ -2,15 +2,20 @@ let numberOfPeople = Int(readLine()!)!
 let people = readLine()!.split(separator: " ").map(String.init).sorted()
 let numberOfInput = Int(readLine()!)!
 var families = [String: ([String], Int)]()
-for _ in stride(from: 1, through: numberOfInput, by: 1){
+
+for _ in 0..<numberOfInput {
     let input = readLine()!.split(separator: " ").map(String.init)
+    
     families[input[0], default: ([], 0)].0.append(input[1])
     families[input[1], default: ([], 0)].1 += 1
 }
+
 var parents = [String]()
 var zeros = [String]()
+
 for person in people {
     let direct = families[person, default: ([], 0)]
+    
     if direct.0.isEmpty {
         parents.append(person)
     }
@@ -18,30 +23,39 @@ for person in people {
         zeros.append(person)
     }
 }
-var children = [String: [String]]()
-while zeros.isEmpty == false {
-    let last = zeros.removeLast()
+
+parents.sort()
+
+var children = [String : [String]]()
+
+while let last =  zeros.popLast() {
     let parents = families[last, default: ([], 0)].0
+    
     for person in parents {
         families[person, default: ([], 0)].1 -= 1
-        let info = families[person, default: ([], 0)]
-        if info.0.count == parents.count - 1 {
+        
+        if families[person, default: ([], 0)].0.count == parents.count - 1 {
             children[person, default: []].append(last)
         }
+        
         if families[person, default: ([], 0)].1 == 0 {
             zeros.append(person)
         }
     }
 }
-print(parents.count)
-parents.sort()
-print(parents.joined(separator: " "))
-var visited = Set<String>()
-for person in people {
-    let children = children[person, default: []].sorted()
-    var result = "\(person) \(children.count)"
-    if children.isEmpty == false {
-        result += " " + children.joined(separator: " ")
-    }
-    print(result)
-}
+
+print(
+    parents.count,
+    parents.joined(separator: " "),
+    people.map {
+        let children = children[$0, default: []].sorted()
+        var result = $0 + " " + String(children.count)
+        
+        if children.isEmpty == false {
+            result += " " + children.joined(separator: " ")
+        }
+        
+        return result
+    }.joined(separator: "\n"),
+    separator: "\n"
+)
