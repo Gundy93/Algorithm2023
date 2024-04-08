f779fbbedb9305c90b5c82e580a93ad38e639ce5
GUNDY <106914201+Gundy93@users.noreply.github.com>
2024-04-08 10:48:03 +0900

    Update 소수 만들기.swift

--- Gundy/Programmers/LV. 1/소수 만들기.swift
+++ Gundy/Programmers/LV. 1/소수 만들기.swift
@@ -1,12 +1,23 @@
 import Foundation
 
 func solution(_ nums:[Int]) -> Int {
-    var result: Int = 0
-
-    for first in 0..<nums.count - 2 {
-        for second in first + 1..<nums.count - 1 {
-            for third in second + 1..<nums.count {
-                if validate(nums[first] + nums[second] + nums[third]) {
+    var result = 0
+    var primeNumbers = Array(repeating: true, count: 2998)
+    
+    for number in 2...2997 {
+        if primeNumbers[number] {
+            for multiple in stride(from: number * number, to: 2998, by: number) {
+                primeNumbers[multiple] = false
+            }
+        }
+    }
+    
+    for first in 0..<nums.count-2 {
+        for second in first+1..<nums.count-1 {
+            let partialSum = nums[first] + nums[second]
+            
+            for third in second+1..<nums.count {
+                if primeNumbers[partialSum + nums[third]] {
                     result += 1
                 }
             }
@@ -14,17 +25,4 @@ func solution(_ nums:[Int]) -> Int {
     }
 
     return result
-}
-
-func validate(_ number: Int) -> Bool {
-    guard number % 2 == 1 else { return false }
-    for divisor in stride(from: 3, through: number, by: 2) {
-        if number % divisor == 0 {
-            return false
-        }
-        if Double(divisor) > sqrt(Double(number)) {
-            break
-        }
-    }
-    return true
 }
