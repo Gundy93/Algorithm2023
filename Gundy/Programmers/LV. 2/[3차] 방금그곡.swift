56a6952a610e762c0621a446c6d4c1100825e99f
GUNDY <106914201+Gundy93@users.noreply.github.com>
2024-04-08 10:48:58 +0900

    Update [3차] 방금그곡.swift

--- Gundy/Programmers/LV. 2/[3차] 방금그곡.swift
+++ Gundy/Programmers/LV. 2/[3차] 방금그곡.swift
@@ -1,36 +1,33 @@
 func solution(_ m:String, _ musicinfos:[String]) -> String {
-    var melody = [String]()
-    for note in m.map(String.init) {
-        if note == "#" {
-            melody[melody.count - 1] = melody[melody.count - 1].lowercased()
-        } else {
-            melody.append(note)
-        }
+    var music = m
+    
+    music = music.replacingOccurrences(of: "C#", with: "c")
+    music = music.replacingOccurrences(of: "D#", with: "d")
+    music = music.replacingOccurrences(of: "F#", with: "f")
+    music = music.replacingOccurrences(of: "G#", with: "g")
+    music = music.replacingOccurrences(of: "A#", with: "a")
+    music = music.replacingOccurrences(of: "B#", with: "b")
+    
+    let musics: [(Int, String, String)] = musicinfos.map {
+        let information = $0.split(separator: ",").map(String.init)
+        let start = information[0].split(separator: ":").map { Int($0)! }
+        let end = information[1].split(separator: ":").map { Int($0)! }
+        let time = (end[0] - start[0]) * 60 + (end[1] - start[1])
+        let name = information[2]
+        var code = information[3]
+        
+        code = code.replacingOccurrences(of: "C#", with: "c")
+        code = code.replacingOccurrences(of: "D#", with: "d")
+        code = code.replacingOccurrences(of: "F#", with: "f")
+        code = code.replacingOccurrences(of: "G#", with: "g")
+        code = code.replacingOccurrences(of: "A#", with: "a")
+        code = code.replacingOccurrences(of: "B#", with: "b")
+        
+        let codes = String(repeating: code, count: (time/code.count)+1)
+        
+        return (time, name, String(codes.dropLast(codes.count-time)))
     }
-    let filteredMelody = melody.joined()
-    var result = [(String, Int)]()
-    for musicInfo in musicinfos {
-        let infos = musicInfo.split(separator: ",").map(String.init)
-        let music = infos[3].map(String.init)
-        let startTime = infos[0].split(separator: ":").compactMap { Int($0) }
-        let endTime = infos[1].split(separator: ":").compactMap { Int($0) }
-        let playedTime = 60 * (endTime[0] - startTime[0]) + endTime[1] - startTime[1]
-        var playedMusic = [String]()
-        var index = 0
-        while playedMusic.count < playedTime {
-            let note = music[index % music.count]
-            if note == "#" {
-                playedMusic[playedMusic.count - 1] = playedMusic[playedMusic.count - 1].lowercased()
-            } else {
-                playedMusic.append(note)
-            }
-            index += 1
-        }
-        if music[index % music.count] == "#" {
-            playedMusic[playedMusic.count - 1] = playedMusic[playedMusic.count - 1].lowercased()
-        }
-        guard playedMusic.joined().contains(filteredMelody) else { continue }
-        result.append((infos[2], playedTime))
-    }
-    return result.isEmpty ? "(None)" : result.sorted { $0.1 > $1.1 }[0].0
+    .filter { $0.2.contains(music) }
+    
+    return musics.min { $0.0 > $1.0 }?.1 ?? "(None)"
 }
