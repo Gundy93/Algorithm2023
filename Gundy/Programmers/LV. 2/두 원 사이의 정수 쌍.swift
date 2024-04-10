a500def3e9664c11a75b1d3933d61ab95a763b3e
GUNDY <106914201+Gundy93@users.noreply.github.com>
2024-04-10 11:12:32 +0900

    Update 두 원 사이의 정수 쌍.swift

--- Gundy/Programmers/LV. 2/두 원 사이의 정수 쌍.swift
+++ Gundy/Programmers/LV. 2/두 원 사이의 정수 쌍.swift
@@ -1,11 +1,19 @@
 import Foundation
 
 func solution(_ r1:Int, _ r2:Int) -> Int64 {
+    let small = Double(r1*r1)
+    let big = Double(r2*r2)
     var result = 0
+    
     for x in 1...r2 {
-        let start = x <= r1 ? Int(ceil(sqrt(Double(r1 * r1 - x * x)))) : 0
-        let end = Int(floor(sqrt(Double(r2 * r2 - x * x))))
-        result += end - start + 1
+        let powX = Double(x*x)
+        
+        if x < r1 {
+            result += Int(sqrt(big - powX) - ceil(sqrt(small - powX))) + 1
+        } else {
+            result += Int(sqrt(big - powX)) + 1
+        }
     }
+    
     return Int64(result * 4)
 }
