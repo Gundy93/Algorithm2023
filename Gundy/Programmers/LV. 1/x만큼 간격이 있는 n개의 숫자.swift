f939d3b2958588c3f5361818f6525e238b82969a
GUNDY <106914201+Gundy93@users.noreply.github.com>
2024-04-10 11:11:40 +0900

    Update x만큼 간격이 있는 n개의 숫자.swift

--- Gundy/Programmers/LV. 1/x만큼 간격이 있는 n개의 숫자.swift
+++ Gundy/Programmers/LV. 1/x만큼 간격이 있는 n개의 숫자.swift
@@ -1,4 +1,5 @@
 func solution(_ x:Int, _ n:Int) -> [Int64] {
     guard x != 0 else { return Array(repeating: Int64(x), count: n) }
-    return stride(from: x, through: x * n, by: x).map({ Int64($0) })
+    
+    return stride(from: x, through: x * n, by: x).map(Int64.init)
 }
