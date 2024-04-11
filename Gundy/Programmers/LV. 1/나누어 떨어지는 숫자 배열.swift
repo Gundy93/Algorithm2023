e3bca1f1933b3c62c0f174a9d7c7b914181858ec
GUNDY <106914201+Gundy93@users.noreply.github.com>
2024-04-11 11:31:02 +0900

    Update 나누어 떨어지는 숫자 배열.swift

--- Gundy/Programmers/LV. 1/나누어 떨어지는 숫자 배열.swift
+++ Gundy/Programmers/LV. 1/나누어 떨어지는 숫자 배열.swift
@@ -1,4 +1,5 @@
 func solution(_ arr:[Int], _ divisor:Int) -> [Int] {
-    let numbers: [Int] = arr.filter({ $0 % divisor == 0 }).sorted()
-    return numbers.isEmpty ? [-1] : numbers
+    let result = arr.filter { $0 % divisor == 0 }.sorted()
+    
+    return result.isEmpty ? [-1] : result
 }
