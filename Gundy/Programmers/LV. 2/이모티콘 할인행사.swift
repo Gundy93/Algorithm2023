8b52c33e33f881152e13cd41f6da0c14fae5649f
GUNDY <106914201+Gundy93@users.noreply.github.com>
2024-04-12 11:23:51 +0900

    Update 이모티콘 할인행사.swift

--- Gundy/Programmers/LV. 2/이모티콘 할인행사.swift
+++ Gundy/Programmers/LV. 2/이모티콘 할인행사.swift
@@ -1,30 +1,47 @@
+import Foundation
+
 func solution(_ users:[[Int]], _ emoticons:[Int]) -> [Int] {
-    var result: (emoticonPlus: Int, sales: Int) = (0, 0)
-    func recursion(discounts: [Int]) {
+    var result = [0, 0]
+    var discounts = [Int]()
+    
+    func backtracking() {
         guard discounts.count < emoticons.count else {
-            var partialResult: (emoticonPlus: Int, sales: Int) = (0, 0)
-            for user in users {
-                var sales = 0
-                for index in 0..<discounts.count {
-                    if discounts[index] >= user[0] {
-                        sales += emoticons[index] * (100 - discounts[index]) / 100
-                    }
-                }
-                if sales >= user[1] {
-                    partialResult.emoticonPlus += 1
-                } else {
-                    partialResult.sales += sales
-                }
-            }
-            if partialResult > result {
-                result = partialResult
-            }
+            countSales()
             return
         }
+        
         for discount in [10, 20, 30, 40] {
-            recursion(discounts: discounts + [discount])
+            discounts.append(discount)
+            backtracking()
+            discounts.removeLast()
         }
     }
-    recursion(discounts: [])
-    return [result.emoticonPlus, result.sales]
+    
+    func countSales() {
+        var partialResult = [0, 0]
+        
+        for user in users {
+            var sales = 0
+            
+            for index in 0..<discounts.count {
+                if discounts[index] >= user[0] {
+                    sales += emoticons[index] * (100 - discounts[index]) / 100
+                }
+            }
+            
+            if sales >= user[1] {
+                partialResult[0] += 1
+            } else {
+                partialResult[1] += sales
+            }
+        }
+        
+        if partialResult[0] > result[0] || partialResult[0] == result[0] && partialResult[1] > result[1] {
+            result = partialResult
+        }
+    }
+    
+    backtracking()
+    
+    return result
 }
