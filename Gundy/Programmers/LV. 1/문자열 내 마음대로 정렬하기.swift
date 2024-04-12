50517f5f50fd9bf93239e46d28bbfeaee063352d
GUNDY <106914201+Gundy93@users.noreply.github.com>
2024-04-12 11:23:20 +0900

    Update 문자열 내 마음대로 정렬하기.swift

--- Gundy/Programmers/LV. 1/문자열 내 마음대로 정렬하기.swift
+++ Gundy/Programmers/LV. 1/문자열 내 마음대로 정렬하기.swift
@@ -1,9 +1,5 @@
 func solution(_ strings:[String], _ n:Int) -> [String] {
-    let text: [String] = strings.map({ $0.map({ String($0) })[n] })
-    return strings.sorted(by: {
-        if text[strings.firstIndex(of: $0)!] == text[strings.firstIndex(of: $1)!] {
-            return $0 < $1
-        }
-        return text[strings.firstIndex(of: $0)!] < text[strings.firstIndex(of: $1)!]
-    })
+    let strings = strings.sorted().map { $0.map(String.init) }
+    
+    return strings.sorted { $0[n] < $1[n] }.map { $0.joined() }
 }
