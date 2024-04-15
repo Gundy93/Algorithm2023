6da8b2fba7d07521afe6b94c255d49bb79bbe8ca
GUNDY <106914201+Gundy93@users.noreply.github.com>
2024-04-15 11:34:08 +0900

    Update 개인정보 수집 유효기간.swift

--- Gundy/Programmers/LV. 1/개인정보 수집 유효기간.swift
+++ Gundy/Programmers/LV. 1/개인정보 수집 유효기간.swift
@@ -1,58 +1,29 @@
-struct Day: Comparable {
-    var year: Int
-    var month: Int {
-        didSet {
-            year += month%12 == 0 ? month/12 - 1 : month/12
-            month = month%12 == 0 ? 12 : month%12
-        }
-    }
-    var day: Int
-    
-    static func < (lhs: Day, rhs: Day) -> Bool {
-        (lhs.year, lhs.month, lhs.day) < (rhs.year, rhs.month, rhs.day)
-    }
-}
+import Foundation
 
 func solution(_ today:String, _ terms:[String], _ privacies:[String]) -> [Int] {
-    let today: Day = {
-        let date = today.split(separator: ".").map { Int($0)! }
+    let today = toDays(today)
+    var termDays = [String : Int]()
+    
+    for term in terms {
+        let term = term.split(separator: " ").map(String.init)
         
-        return Day(year: date[0], month: date[1], day: date[2])
-    }()
-    let terms: [String : Int] = {
-        var dictionary = [String : Int]()
-        
-        for term in terms {
-            let term = term.split(separator: " ").map(String.init)
-            
-            dictionary[term[0]] = Int(term[1])!
-        }
-        
-        return dictionary
-    }()
-    let privacies: [Day] = {
-        var dates = [Day]()
-        
-        for privacy in privacies {
-            let privacy = privacy.split(separator: " ").map(String.init)
-            let origin = privacy[0].split(separator: ".").map { Int($0)! }
-            let term = terms[privacy[1]]!
-            var date = Day(year: origin[0], month: origin[1], day: origin[2])
-            
-            date.month += term
-            dates.append(date)
-        }
-        
-        return dates
-    }()
+        termDays[term[0]] = Int(term[1])! * 28
+    }
     
     var result = [Int]()
     
     for index in 0..<privacies.count {
-        if (privacies[index] > today) == false {
-            result.append(index+1)
-        }
+        let privacy = privacies[index].split(separator: " ").map(String.init)
+        let due = toDays(privacy[0]) + termDays[privacy[1]]!
+        
+        if due <= today { result.append(index+1) }
     }
     
     return result
+}
+
+func toDays(_ string: String) -> Int {
+    let numbers = string.split(separator: ".").map { Int($0)! }
+    
+    return numbers[0] * 12 * 28 + numbers[1] * 28 + numbers[2]
 }
