8beea5f4ed531257990d4f37bafb085e11c0e8c4
GUNDY <106914201+Gundy93@users.noreply.github.com>
2024-04-15 11:35:09 +0900

    Update 교점에 별 만들기.swift

--- Gundy/Programmers/LV. 2/교점에 별 만들기.swift
+++ Gundy/Programmers/LV. 2/교점에 별 만들기.swift
@@ -1,43 +1,49 @@
+import Foundation
+
 func solution(_ line:[[Int]]) -> [String] {
-    var points = [Int: [Int]]()
-    var minimumX = Int.max
-    var maximumX = Int.min
-    var minimumY = Int.max
-    var maximumY = Int.min
-    for index in 0...line.count - 2 {
-        let origin = line[index]
-        let (a, b, e) = (origin[0], origin[1], origin[2])
-        for other in index + 1...line.count - 1 {
-            let target = line[other]
-            let (c, d, f) = (target[0], target[1], target[2])
-            guard a * d - b * c != 0 else { continue }
-            let numeratorX = b * f - e * d
-            let numeratorY = e * c - a * f
-            let denominator = a * d - b * c
-            let x = numeratorX / denominator
-            let y = numeratorY / denominator
-            guard Double(numeratorX) / Double(denominator) == Double(x), Double(numeratorY) / Double(denominator) == Double(y) else { continue }
-            points[y, default: []].append(x)
-            if x < minimumX {
-                minimumX = x
-            }
-            if x > maximumX {
-                maximumX = x
-            }
-            if y < minimumY {
-                minimumY = y
-            }
-            if y > maximumY {
-                maximumY = y
-            }
+    var points = [(Int, Int)]()
+    var left = Int.max
+    var right = Int.min
+    var top = Int.min
+    var bottom = Int.max
+    
+    for first in 0..<line.count-1 {
+        for second in first+1..<line.count {
+            let firstNumbers = line[first].map(Double.init)
+            let secondNumbers = line[second].map(Double.init)
+            let (A, B, C) = (firstNumbers[0], firstNumbers[1], firstNumbers[2])
+            let (D, E, F) = (secondNumbers[0], secondNumbers[1], secondNumbers[2])
+            let x = (B*F - C*E)/(A*E - B*D)
+            let y = (A*F - C*D)/(B*D - A*E)
+            
+            guard x.isNaN == false,
+                  x.isInfinite == false,
+                  y.isNaN == false,
+                  y.isInfinite == false,
+                  x == Double(Int(x)),
+                  y == Double(Int(y)) else { continue }
+            
+            let row = Int(y)
+            let column = Int(x)
+            
+            left = min(left, column)
+            right = max(right, column)
+            top = max(top, row)
+            bottom = min(bottom, row)
+            points.append((row, column))
         }
     }
-    var result = Array(repeating: Array(repeating: ".", count: maximumX - minimumX + 1), count: maximumY - minimumY + 1)
-    for y in points.keys.sorted() {
-        let xs = points[y]!
-        for x in xs {
-            result[maximumY - y][x - minimumX] = "*"
-        }
+    
+    
+    
+    var result = Array(repeating: Array(repeating: ".", count: right - left + 1), count: top - bottom + 1)
+    
+    for (row, column) in points {
+        let row = top - row
+        let column = column - left
+        
+        result[row][column] = "*"
     }
+    
     return result.map { $0.joined() }
 }
