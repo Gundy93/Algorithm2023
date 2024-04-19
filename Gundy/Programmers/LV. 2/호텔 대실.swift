cadf3ed49fd9f38631e473e11f44c4a4dc8c44d7
GUNDY <106914201+Gundy93@users.noreply.github.com>
2024-04-19 19:26:42 +0900

    Update 호텔 대실.swift

--- Gundy/Programmers/LV. 2/호텔 대실.swift
+++ Gundy/Programmers/LV. 2/호텔 대실.swift
@@ -1,38 +1,35 @@
-extension String {
-    func isFasterThan(_ time: Self) -> Bool {
-        let lhs = split(separator: ":").compactMap { Int($0) }
-        let rhs = time.split(separator: ":").compactMap { Int($0) }
-        return lhs[0] < rhs[0] || lhs[0] == rhs[0] && lhs[1] <= rhs[1]
-    }
-
-    func nextTime() -> Self {
-        let time = split(separator: ":").compactMap { Int($0) }
-        let hour = time[0]
-        let minute = time[1]
-        return minute < 50 ? "\(hour):\(minute + 10)" : "\(hour + 1):\(minute - 50)"
-    }
-}
+import Foundation
 
 func solution(_ book_time:[[String]]) -> Int {
-    guard book_time.count > 1 else {
-        return 1
-    }
-    let bookTime = book_time.sorted { $0[0].isFasterThan($1[0]) }
-    var rooms = [bookTime[0][1].nextTime()]
-    for timeIndex in 1..<bookTime.count {
-        var isCheckedIn = false
-        for roomIndex in 0..<rooms.count {
-            guard rooms[roomIndex].isFasterThan(bookTime[timeIndex][0]) else {
-                continue
+    var bookTimes = book_time.map(toTimes).sorted(by: <)
+    var result = 0
+    
+    while bookTimes.isEmpty == false {
+        result += 1
+        
+        var last = -10
+        var stack = [(Int, Int)]()
+        
+        for bookTime in bookTimes {
+            if last+10 > bookTime.0 {
+                stack.append(bookTime)
+            } else {
+                last = bookTime.1
             }
-            rooms[roomIndex] = bookTime[timeIndex][1].nextTime()
-            isCheckedIn = true
-            break
         }
-        guard isCheckedIn == false else {
-            continue
-        }
-        rooms.append(bookTime[timeIndex][1].nextTime())
+        
+        bookTimes = stack
     }
-    return rooms.count
+    
+    return result
+}
+
+func toTimes(_ times: [String]) -> (Int, Int) {
+    (toTime(times[0]), toTime(times[1]))
+}
+
+func toTime(_ time: String) -> Int {
+    let time = time.split(separator: ":").map { Int($0)! }
+    
+    return time[0]*60 + time[1]
 }
