df1341e5075caf6841c04c91dee9f99f9e4ffdf9
GUNDY <106914201+Gundy93@users.noreply.github.com>
2024-04-19 19:27:09 +0900

    Update 풍선 터트리기.swift

--- Gundy/Programmers/LV. 3/풍선 터트리기.swift
+++ Gundy/Programmers/LV. 3/풍선 터트리기.swift
@@ -1,26 +1,24 @@
+import Foundation
+
 func solution(_ a:[Int]) -> Int {
+    var lefts = Array(repeating: Int.max, count: a.count)
+    var rights = Array(repeating: Int.max, count: a.count)
+    
+    for index in 1..<a.count {
+        lefts[index] = min(a[index-1], lefts[index-1])
+    }
+    
+    for index in stride(from: a.count-2, through: 0, by: -1) {
+        rights[index] = min(a[index+1], rights[index+1])
+    }
+    
     var result = 0
-    var minimums = Array(repeating: [Int.max, Int.max], count: a.count)
-    for index in 0...a.count - 2 {
-        let balloon = a[index]
-        minimums[index + 1][0] = min(minimums[index][0], balloon)
-    }
-    for index in stride(from: a.count - 1, to: 0, by: -1) {
-        let balloon = a[index]
-        minimums[index - 1][1] = min(minimums[index][1], balloon)
-    }
-    for index in 0...a.count - 1 {
-        let balloon = a[index]
-        switch index {
-        case 0, a.count - 1:
+    
+    for index in 0..<a.count {
+        if a[index] < lefts[index] || a[index] < rights[index] {
             result += 1
-        default:
-            let leftMin = minimums[index][0]
-            let rightMin = minimums[index][1]
-            if (leftMin < balloon && rightMin < balloon) == false {
-                result += 1
-            }
         }
     }
+    
     return result
 }
