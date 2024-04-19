6d7d34d0fff2e9d1ef1ad412973834d95b32e7a6
GUNDY <106914201+Gundy93@users.noreply.github.com>
2024-04-19 19:26:16 +0900

    Update 카드 뭉치.swift

--- Gundy/Programmers/LV. 1/카드 뭉치.swift
+++ Gundy/Programmers/LV. 1/카드 뭉치.swift
@@ -1,16 +1,23 @@
+import Foundation
+
 func solution(_ cards1:[String], _ cards2:[String], _ goal:[String]) -> String {
-    var first: [String] = cards1.reversed()
-    var second: [String] = cards2.reversed()
-    for text in goal {
-        if first.last == text {
-            first.removeLast()
-            continue
+    var left = 0
+    var right = 0
+    var index = 0
+    
+    while index < goal.count {
+        if left < cards1.count,
+           goal[index] == cards1[left] {
+            left += 1
+            index += 1
+        } else if right < cards2.count,
+                  goal[index] == cards2[right] {
+            right += 1
+            index += 1
+        } else {
+            return "No"
         }
-        if second.last == text {
-            second.removeLast()
-            continue
-        }
-        return "No"
     }
+    
     return "Yes"
 }
