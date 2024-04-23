d49cdeb366dcb02a0b408a97e079ae2ff6a735c8
GUNDY <106914201+Gundy93@users.noreply.github.com>
2024-04-23 11:19:51 +0900

    Update 성격 유형 검사하기.swift

--- Gundy/Programmers/LV. 1/성격 유형 검사하기.swift
+++ Gundy/Programmers/LV. 1/성격 유형 검사하기.swift
@@ -1,18 +1,26 @@
 import Foundation
 
 func solution(_ survey:[String], _ choices:[Int]) -> String {
-    var points = [Character : Int]()
+    var score = [String : Int]()
     
-    for (survey, choice) in zip(survey, choices) {
+    for (friends, choice) in zip(survey, choices) {
         switch choice {
-        case ...3:
-            points[survey.first!, default: 0] += abs(4-choice)
-        case 4:
-            continue
+        case 1...3:
+            score[String(friends.first!), default: 0] += abs(choice-4)
+        case 5...7:
+            score[String(friends.last!), default: 0] += abs(choice-4)
         default:
-            points[survey.last!, default: 0] += abs(4-choice)
+            break
         }
     }
     
-    return String(["RT", "CF", "JM", "AN"].map { $0.sorted { points[$0, default: 0] > points[$1, default: 0] } }.map { $0.first! })
+    var result = ""
+    
+    for friends in [["R", "T"], ["C", "F"], ["J", "M"], ["A", "N"]] {
+        let friend = friends.sorted { score[$0] == score[$1] ? $0 < $1 : score[$0, default: 0] > score[$1, default: 0] }.first!
+        
+        result += friend
+    }
+    
+    return result
 }
