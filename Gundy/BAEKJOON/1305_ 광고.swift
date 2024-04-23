9e77c7b105c28aeb1083a677d9d3dfd7e6bd04a3
GUNDY <106914201+Gundy93@users.noreply.github.com>
2024-04-23 14:31:38 +0900

    Create 1305_ 광고.swift

---
+++ Gundy/BAEKJOON/1305_ 광고.swift
@@ -0,0 +1,32 @@
+func getFailure(of array: [String]) -> [Int] {
+    var failure = Array(repeating: 0, count: array.count)
+    var index = 1
+    var length = 0
+    
+    while index < array.count {
+        if array[index] == array[length] {
+            length += 1
+            failure[index] = length
+            index += 1
+        } else {
+            if length == 0 {
+                failure[index] = 0
+                index += 1
+            } else {
+                length = failure[length-1]
+            }
+        }
+    }
+    
+    return failure
+}
+
+func solution() {
+    let maxLength = Int(readLine()!)!
+    let text = readLine()!.map(String.init)
+    let failure = getFailure(of: text)
+    
+    print(maxLength - failure.last!)
+}
+
+solution()
