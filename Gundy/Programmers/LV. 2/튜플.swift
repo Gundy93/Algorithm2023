4f6f4870adbec2b0f22194163afe1a1f55ea8296
GUNDY <106914201+Gundy93@users.noreply.github.com>
2024-04-24 10:33:34 +0900

    Update 튜플.swift

--- Gundy/Programmers/LV. 2/튜플.swift
+++ Gundy/Programmers/LV. 2/튜플.swift
@@ -1,36 +1,19 @@
+import Foundation
+
 func solution(_ s:String) -> [Int] {
-    var stack: [[Int]] = []
-    var temporary: String = ""
-    for text in s {
-        switch text {
-        case "}":
-            let numbers: [Int] = temporary.split(separator: " ").compactMap({ Int($0) })
-            guard numbers.isEmpty == false else { continue }
-            stack.append(numbers)
-            temporary = ""
-        case "{":
-            continue
-        case ",":
-            temporary += " "
-        default:
-            temporary += String(text)
-        }
-    }
-    let numbersList: [[Int]] = stack.sorted(by: { $0.count < $1.count })
-    var result: [Int] = []
-    var resultCount: [Int : Int] = [:]
-    for numbers in numbersList {
-        var count: [Int : Int] = [:]
-        for number in numbers {
-            count[number, default: 0] += 1
-        }
-        for number in Set(numbers) {
-            if count[number] != resultCount[number] {
+    var checked = Set<Int>()
+    var result = [Int]()
+    var tuples = s.split(separator: "{").flatMap { $0.split(separator: "}") }.map { $0.split(separator: ",").compactMap { Int($0) } }.filter { $0.isEmpty == false }
+    
+    for tuple in tuples.sorted(by: { $0.count < $1.count }) {
+        for number in tuple {
+            if checked.contains(number) == false {
+                checked.insert(number)
                 result.append(number)
-                resultCount[number, default: 0] += 1
                 break
             }
         }
     }
+    
     return result
 }
