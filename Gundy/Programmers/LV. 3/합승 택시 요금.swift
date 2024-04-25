d1e8e8efe8b2c533e2f0e6af69aa92a48272ff28
GUNDY <106914201+Gundy93@users.noreply.github.com>
2024-04-25 19:45:41 +0900

    Update 합승 택시 요금.swift

--- Gundy/Programmers/LV. 3/합승 택시 요금.swift
+++ Gundy/Programmers/LV. 3/합승 택시 요금.swift
@@ -1,27 +1,28 @@
+import Foundation
+
 func solution(_ n:Int, _ s:Int, _ a:Int, _ b:Int, _ fares:[[Int]]) -> Int {
-    var prices = Array(repeating: Array(repeating: 100000 * n, count:n + 1), count: n + 1)
-    var result = Int.max
-    for number in 1...n {
-        prices[number][number] = 0
-    }
+    var dp = Array(repeating: Array(repeating: n*100000, count: n+1), count: n+1)
+    
     for fare in fares {
-        prices[fare[0]][fare[1]] = fare[2]
-        prices[fare[1]][fare[0]] = fare[2]
+        dp[fare[0]][fare[1]] = fare[2]
+        dp[fare[1]][fare[0]] = fare[2]
     }
-    for first in 1...n {
-        for second in 1...n {
-            for third in 1...n {
-                if prices[second][third] > prices[second][first] + prices[first][third] {
-                    prices[second][third] = prices[second][first] + prices[first][third]
-                }
+    
+    for mid in 1...n {
+        dp[mid][mid] = 0
+        
+        for start in 1...n where mid != start {
+            for end in 1...n where mid != end && start != end {
+                dp[start][end] = min(dp[start][end], dp[start][mid] + dp[mid][end])
             }
         }
     }
-    for c in 1...n {
-        let price = prices[s][c] + prices[c][a] + prices[c][b]
-        if result > price {
-            result = price
-        }
+    
+    var result = Int.max
+    
+    for mid in 1...n {
+        result = min(result, dp[s][mid] + dp[mid][a] + dp[mid][b])
     }
+    
     return result
 }
