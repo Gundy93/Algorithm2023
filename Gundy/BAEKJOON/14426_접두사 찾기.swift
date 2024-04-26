3f85881bb1f2b5d6d8b16d201007d8803fb19d44
GUNDY <106914201+Gundy93@users.noreply.github.com>
2024-04-26 14:31:28 +0900

    Update 14426_접두사 찾기.swift

--- Gundy/BAEKJOON/14426_접두사 찾기.swift
+++ Gundy/BAEKJOON/14426_접두사 찾기.swift
@@ -1,51 +1,31 @@
-struct Trie {
-    private var tree = Array(repeating: Array(repeating: 0, count: 27), count: 500*10000+1)
-    private var next = 1
-    
-    mutating func insert(_ newElement: String) {
-        var current = 0
-        
-        for character in newElement {
-            let index = Int(character.asciiValue! - 97)
-            
-            if tree[current][index] == 0 {
-                tree[current][index] = next
-                next += 1
-            }
-            
-            current = tree[current][index]
-        }
-        
-        tree[current][26] = 1
-    }
-    
-    func contains(_ element: String) -> Bool {
-        var current = 0
-        
-        for character in element {
-            let index = Int(character.asciiValue! - 97)
-            
-            if tree[current][index] == 0 { return false }
-            
-            current = tree[current][index]
-        }
-        
-        return true
-    }
-}
-
 func solution() {
     let counts = readLine()!.split(separator: " ").map { Int($0)! }
-    var trie = Trie()
+    var words = [String]()
     
     for _ in 0..<counts[0] {
-        trie.insert(readLine()!)
+        words.append(readLine()!)
     }
+    
+    words.sort()
     
     var result = 0
     
     for _ in 0..<counts[1] {
-        if trie.contains(readLine()!) { result += 1 }
+        let word = readLine()!
+        var start = 0
+        var end = counts[0]-1
+        
+        while start < end {
+            let index = (start + end + 1)/2
+            
+            if words[index] > word {
+                end = index-1
+            } else {
+                start = index
+            }
+        }
+        
+        if words[start].hasPrefix(word) || words[min(start+1, counts[0]-1)].hasPrefix(word) { result += 1 }
     }
     
     print(result)
